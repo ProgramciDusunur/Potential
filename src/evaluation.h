f8ab71438115695d51059889e4b7f58eea77e843
ProgramciDusunur <lerenarazl@gmail.com>
2024-07-26 02:24:24 +0300

    isolated pawn test failed, some more penalties for isolated pawns at the end of the game -------------------------------------- Elo   | -3.22 +- 9.14 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -3.09 (-2.94, 2.94) [0.00, 10.00] Games | N: 4316 W: 1725 L: 1765 D: 826 Penta | [343, 336, 828, 320, 331] https://programcidusunur.pythonanywhere.com/test/19/ --------------------------------------

--- src/evaluation.h
+++ src/evaluation.h
@@ -201,7 +201,7 @@ static const int double_pawn_penalty_endgame = -10;
 
 // isolated pawn penalty
 static const int isolated_pawn_penalty_middle = -10;
-static const int isolated_pawn_penalty_endgame = -5;
+static const int isolated_pawn_penalty_endgame = -15;
 
 // passed pawn bonus
 static const int passed_pawn_bonus_middle[8] = { 0, 0, 0, 5, 10, 15, 20, 25};
