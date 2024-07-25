5cf1dfc950401ba3fa5603e5508f5ad623539faf
ProgramciDusunur <lerenarazl@gmail.com>
2024-07-25 16:14:00 +0300

    fix major compiler errors

--- src/board_constants.h
+++ src/board_constants.h
@@ -53,7 +53,7 @@ enum {
     allMoves, onlyCaptures
 };
 
-char static *squareToCoordinates[] = {
+static char *squareToCoordinates[] = {
         "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
         "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
         "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
@@ -65,9 +65,9 @@ char static *squareToCoordinates[] = {
 };
 
 // ASCII pieces
-char static asciiPieces[12] = "PNBRQKpnbrqk";
+static char asciiPieces[12] = "PNBRQKpnbrqk";
 // promoted pieces
-char static promotedPieces[] = {
+static char promotedPieces[] = {
         [Q] = 'q',
         [R] = 'r',
         [B] = 'b',
@@ -78,10 +78,10 @@ char static promotedPieces[] = {
         [n] = 'n'
 };
 // unicode ieces
-char static *unicodePieces[12] = {"♙", "♘", "♗", "♖", "♕", "♔",
+static char *unicodePieces[12] = {"♙", "♘", "♗", "♖", "♕", "♔",
                                   "♟", "♞", "♝", "♜", "♛", "♚"};
 // convert ASCII character pieces to encoded constants
-int static charPieces[] = {
+static int charPieces[] = {
         ['P'] = P,
         ['N'] = N,
         ['B'] = B,
@@ -98,7 +98,7 @@ int static charPieces[] = {
 
 
 // MVV LVA [attacker][victim]
-int static mvvLva[12][12] = {
+static int mvvLva[12][12] = {
         105, 205, 305, 405, 505, 605, 105, 205, 305, 405, 505, 605,
         104, 204, 304, 404, 504, 604, 104, 204, 304, 404, 504, 604,
         103, 203, 303, 403, 503, 603, 103, 203, 303, 403, 503, 603,
@@ -118,43 +118,43 @@ int static mvvLva[12][12] = {
 
 
 // mask H file
-const static U64 notHFile = 9187201950435737471ULL;
+static const U64 notHFile = 9187201950435737471ULL;
 // mask A file
-const static U64 notAFile = 18374403900871474942ULL;
+static const U64 notAFile = 18374403900871474942ULL;
 // mask H rank
-const static U64 not8Rank = 18446744073709551360ULL;
+static const U64 not8Rank = 18446744073709551360ULL;
 // mask A rank
-const static U64 not1Rank = 72057594037927935ULL;
+static const U64 not1Rank = 72057594037927935ULL;
 // mask 8 rank and AB files
-const static U64 not8RankAndABFile = 18229723555195321344ULL;
+static const U64 not8RankAndABFile = 18229723555195321344ULL;
 // mask 1 rank and GH files
-const static U64 not1RankAndGHFile = 17802464409370431ULL;
+static const U64 not1RankAndGHFile = 17802464409370431ULL;
 // mask 1 and 2 ranks and H file
-const static U64 not1And2RankHFile = 140185576636287ULL;
+static const U64 not1And2RankHFile = 140185576636287ULL;
 // mask 8 and 7 ranks and A file
-const static U64 not8And7RankAFile = 18374403900871409664ULL;
+static const U64 not8And7RankAFile = 18374403900871409664ULL;
 // mask 8 and 7 ranks and H file
-const static U64 not8And7RankHFile = 9187201950435704832ULL;
+static const U64 not8And7RankHFile = 9187201950435704832ULL;
 // mask 1 and 2 ranks and A file
-const static U64 not1And2RanksAFile = 280371153272574ULL;
+static const U64 not1And2RanksAFile = 280371153272574ULL;
 //mask 1 rank and AB files
-const static U64 not1RankAndABFile = 71209857637481724ULL;
+static const U64 not1RankAndABFile = 71209857637481724ULL;
 // mask 8 rank and GH files
-const static U64 not8RankAndGHFile = 4557430888798830336ULL;
+static const U64 not8RankAndGHFile = 4557430888798830336ULL;
 // not A file constant
-const static U64 not_a_file = 18374403900871474942ULL;
+static const U64 not_a_file = 18374403900871474942ULL;
 // not H file constant
-const static U64 not_h_file = 9187201950435737471ULL;
+static const U64 not_h_file = 9187201950435737471ULL;
 // not H rank and A file
-const static U64 notAFileAndHRank = 35887507618889599ULL;
+static const U64 notAFileAndHRank = 35887507618889599ULL;
 // not 8 Rank and A file
-const static U64 not8RankAndAFile = 18374403900871474688ULL;
+static const U64 not8RankAndAFile = 18374403900871474688ULL;
 // not 8 Rank and H file
-const static U64 not8RankAndHFile = 9187201950435737344ULL;
+static const U64 not8RankAndHFile = 9187201950435737344ULL;
 // not 1 Rank and A file
-const static U64 not1RankAndAFile = 71775015237779198ULL;
+static const U64 not1RankAndAFile = 71775015237779198ULL;
 
-const static int get_rank[64] = {
+static const  int get_rank[64] = {
         7, 7, 7, 7, 7, 7, 7, 7,
         6, 6, 6, 6, 6, 6, 6, 6,
         5, 5, 5, 5, 5, 5, 5, 5,
