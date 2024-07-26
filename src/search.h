66e116bc1b830553d10c215be20604b806b6a806
ProgramciDusunur <lerenarazl@gmail.com>
2024-07-27 00:31:50 +0300

    previous test failed, try again according to the depth --------------------------------------------------- Elo   | -42.15 +- 21.68 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -3.32 (-2.94, 2.94) [0.00, 10.00] Games | N: 762 W: 267 L: 359 D: 136 Penta | [77, 63, 160, 37, 44] https://programcidusunur.pythonanywhere.com/test/35/ ---------------------------------------------------

--- src/search.h
+++ src/search.h
@@ -610,7 +610,7 @@ static inline int negamax(int alpha, int beta, int depth, board* position) {
             int lmrReduction = getLmrReduction(depth, position->ply, pvNode, improving);
             if (isQuiet) {
                 // Reduce More
-                if (!improving && quietMoves >= 16) {
+                if (!improving && quietMoves >= 8 * depth * depth) {
                     lmrReduction += 1;
                 }
                 if (!pvNode && quietMoves >= 4) {
