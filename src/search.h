92e56ab394dcdcce3470f8a807bfbf856d2d42de
ProgramciDusunur <lerenarazl@gmail.com>
2024-08-01 00:48:37 +0300

    prune more late quiet moves when not improving

--- src/search.h
+++ src/search.h
@@ -409,7 +409,7 @@ static inline int negamax(int alpha, int beta, int depth, board* position) {
 
     position->staticEval[position->ply] = static_eval;
 
-    /*if(in_check)
+    if(in_check)
         improving = false;
     else if (position->staticEval[position->ply-2] != noEval) {
         improving = position->staticEval[position->ply] > position->staticEval[position->ply-2];
@@ -418,7 +418,7 @@ static inline int negamax(int alpha, int beta, int depth, board* position) {
         improving = position->staticEval[position->ply] > position->staticEval[position->ply-4];
     }
     else
-        improving = true;*/
+        improving = true;
 
     //printf("static eval calculated %d\n", position->staticEval[position->ply]);
 
@@ -549,10 +549,10 @@ static inline int negamax(int alpha, int beta, int depth, board* position) {
         }
 
         bool isNotMated = alpha > -mateScore + maxPly;
-
-        int lmpBase = 4;
+        // 10512906 nodes 1316000 nps
+        int lmpBase = 4 - improving;
         int lmpMultiplier = 2;
-        int lmpThreshold = (lmpBase + lmpMultiplier * depth * depth);
+        int lmpThreshold = ((lmpBase) + lmpMultiplier * depth * depth);
         // Late Move Pruning (~13 Elo)
         if (!rootNode && isQuiet &&
             isNotMated &&
