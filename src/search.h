c0aa46b0d9330609f709b082c852b6dfd5c4d7eb
Eren Araz <77514455+ProgramciDusunur@users.noreply.github.com>
2024-07-31 14:22:18 +0300

    correct the elo brought by LMP

--- src/search.h
+++ src/search.h
@@ -553,7 +553,7 @@ static inline int negamax(int alpha, int beta, int depth, board* position) {
         int lmpBase = 4;
         int lmpMultiplier = 2;
         int lmpThreshold = (lmpBase + lmpMultiplier * depth * depth);
-        // Late Move Pruning (~13 Elo)
+        // Late Move Pruning (~18 Elo)
         if (!rootNode && isQuiet &&
             isNotMated &&
             legal_moves>= lmpThreshold) {
