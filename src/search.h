4078fddb0f91f8b0dd67ddfdc2d4807b153d984f
ProgramciDusunur <lerenarazl@gmail.com>
2024-08-03 18:12:01 +0300

    previous test don't response, try with new searched move value --------------------------------------------------- Elo   | -0.93 +- 12.67 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -1.13 (-2.94, 2.94) [0.00, 10.00] Games | N: 2244 W: 893 L: 899 D: 452 Penta | [171, 189, 408, 183, 171] https://programcidusunur.pythonanywhere.com/test/97/ ---------------------------------------------------

--- src/search.h
+++ src/search.h
@@ -653,7 +653,7 @@ static inline int negamax(int alpha, int beta, int depth, board* position) {
 
 
             } else {
-                if (pvNode && moves_searched >= 5) {
+                if (pvNode && moves_searched >= 8) {
                     lmrReduction -= 1;
                 }
             }
