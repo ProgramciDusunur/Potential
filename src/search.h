788165dce9db0087a2d652aea130f373740e6bf9
ProgramciDusunur <lerenarazl@gmail.com>
2024-08-11 16:06:13 +0300

    previous test failed, reduce more when on non pv nodes and searched capture moves >= 2 ----------------------------------------------------- Elo   | -0.68 +- 7.58 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -2.95 (-2.94, 2.94) [0.00, 10.00] Games | N: 6174 W: 2459 L: 2471 D: 1244 Penta | [471, 485, 1184, 479, 468] https://programcidusunur.pythonanywhere.com/test/133/ -----------------------------------------------------

--- src/search.h
+++ src/search.h
@@ -661,7 +661,7 @@ static inline int negamax(int alpha, int beta, int depth, board* position) {
 
 
             } else {
-                if (!pvNode && captureMoves >= 3) {
+                if (!pvNode && captureMoves >= 2) {
                     lmrReduction += 1;
                 }
             }
