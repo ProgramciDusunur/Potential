8d5326a5317382624af6e8a6855aa36e29373ca2
ProgramciDusunur <lerenarazl@gmail.com>
2024-08-12 22:49:43 +0300

    previous test failed, even if the test pass shouldn't merge it, need to fix the loss of time ----------------------------------------------------- Elo   | 0.67 +- 2.90 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -2.95 (-2.94, 2.94) [0.00, 5.00] Games | N: 41350 W: 16712 L: 16632 D: 8006 Penta | [3080, 3128, 8186, 3194, 3087] https://programcidusunur.pythonanywhere.com/test/145/ -----------------------------------------------------

--- src/time.h
+++ src/time.h
@@ -46,7 +46,7 @@ static int stopped = 0;
 static inline void resetTimeControl() {
     // reset timing
     quit = 0;
-    movestogo = 15;
+    movestogo = 40;
     movetime = -1;
     time = -1;
     inc = 0;
