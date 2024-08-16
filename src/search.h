8ea25245ce3035f9324437d683ff0ebb55c45554
ProgramciDusunur <lerenarazl@gmail.com>
2024-08-16 11:25:49 +0300

    previous test failed, insert pvNode into the formula ----------------------------------------------------- Elo   | -4.42 +- 10.00 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -2.95 (-2.94, 2.94) [0.00, 10.00] Games | N: 3540 W: 1352 L: 1397 D: 791 Penta | [273, 309, 621, 324, 243] https://programcidusunur.pythonanywhere.com/test/163/ -----------------------------------------------------

--- src/search.h
+++ src/search.h
@@ -448,9 +448,9 @@ static inline int negamax(int alpha, int beta, int depth, board* position, time*
 
 
     // Reverse futility pruning
-    if (depth < 4 && canPrune && abs(beta - 1) > -infinity + 100) {
+    if (depth < 4 && in_check && abs(beta - 1) > -infinity + 100) {
         // improving factor
-        int improvingMargin = (int)((position->improvingRate[position->ply] / 2 )* 20);
+        int improvingMargin = (int)((position->improvingRate[position->ply] / 2 ) * (20 - pvNode));
 
         // define evaluation margin
         int eval_margin = (100 + improvingMargin) * depth;
