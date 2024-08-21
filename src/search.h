5f1fc1a07203ca6540cabca8716cccbe17ff8689
ProgramciDusunur <lerenarazl@gmail.com>
2024-08-21 19:26:25 +0300

    previous test failed, try new approach ----------------------------------------------------- Elo   | -2.61 +- 8.93 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -2.98 (-2.94, 2.94) [0.00, 10.00] Games | N: 4524 W: 1753 L: 1787 D: 984 Penta | [347, 397, 800, 379, 339] https://programcidusunur.pythonanywhere.com/test/194/ -----------------------------------------------------

--- src/search.h
+++ src/search.h
@@ -651,7 +651,7 @@ static inline int negamax(int alpha, int beta, int depth, board* position, time*
                     lmrReduction -= 1;
                 }
 
-                if (in_check) {
+                if (in_check && depth > 10) {
                     lmrReduction -= 1;
                 }
 
