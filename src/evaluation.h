25eebaa0d979ad25a74d25d4f1df65e9c917ebc6
ProgramciDusunur <lerenarazl@gmail.com>
2024-09-01 19:32:07 +0300

    previous test failed hard, fix wrong scoring for black ----------------------------------------------------- Elo   | -26.04 +- 18.21 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -2.99 (-2.94, 2.94) [0.00, 10.00] Games | N: 1016 W: 350 L: 426 D: 240 Penta | [81, 105, 188, 77, 57] https://programcidusunur.pythonanywhere.com/test/247/ -----------------------------------------------------

--- src/evaluation.h
+++ src/evaluation.h
@@ -543,7 +543,7 @@ static inline int evaluate(board* position) {
                            int whiteKingDistance = (getLS1BIndex(position->bitboards[K]) - square) / 8;
                            int blackKingDistance = (getLS1BIndex(position->bitboards[k]) - square) / 8;
                            int kingDistance = whiteKingDistance - blackKingDistance;
-                           score += kingDistance * king_distance_bonus;
+                           score -= kingDistance * king_distance_bonus;
                         }
                     }
 
