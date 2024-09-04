b624d7536a1f97489a94cecadf7ba06499d24f3a
ProgramciDusunur <lerenarazl@gmail.com>
2024-09-04 20:41:10 +0300

    previous test failed hard, try with new isolated pawn endgame value ----------------------------------------------------- Elo   | -13.43 +- 13.50 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -3.03 (-2.94, 2.94) [0.00, 10.00] Games | N: 1708 W: 587 L: 653 D: 468 Penta | [113, 176, 319, 156, 90] https://programcidusunur.pythonanywhere.com/test/282/ -----------------------------------------------------

--- src/evaluation.h
+++ src/evaluation.h
@@ -205,13 +205,13 @@ static const int double_pawn_penalty_endgame = -10;
 
 // isolated pawn penalty
 static const int isolated_pawn_penalty_middle = -5;
-static const int isolated_pawn_penalty_endgame = -15;
+static const int isolated_pawn_penalty_endgame = -12;
 static const int isolated_pawn_rank[64] = {
         0, 0, 0, 0, 0, 0, 0, 0,
         4, 4, 4, 4, 4, 4, 4, 4,
         3, 3, 3, 3, 3, 3, 3, 3,
         2, 2, 2, 2, 2, 2, 2, 2,
-        0, 0, 0, 1, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0, 0, 0, 0, 0
