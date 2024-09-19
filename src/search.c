5ae33ccf2c0d79b49aa4b931fb9874eb53571c1f
ProgramciDusunur <lerenarazl@gmail.com>
2024-09-19 23:15:37 +0300

    try null move pruning with improving heuristic

--- src/search.c
+++ src/search.c
@@ -498,8 +498,8 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
 
         // hash the side
         position->hashKey ^= sideKey;
-
-        int R = 3 + (int)(0.1875 * depth);
+        int nullMoveBase = improving ? 4 : 3;
+        int R = nullMoveBase + (int)(0.1875 * depth);
 
         /* search moves with reduced depth to find beta cutoffs
            depth - R where R is a reduction limit */
