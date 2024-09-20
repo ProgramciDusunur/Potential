af8f5302cbd5c77b466eec34d25fd186c5bbc4c2
ProgramciDusunur <lerenarazl@gmail.com>
2024-09-21 02:43:23 +0300

    try futility pruning with improving rate

--- src/search.c
+++ src/search.c
@@ -602,7 +602,8 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
             }
             // Futility pruning
             int futilityMargin = improving ? static_eval + 100 : static_eval + 80;
-            if (canPrune && depth < 4 && futilityMargin <= alpha) {
+            int futilityThreshold = futilityMargin - improvingFactor;
+            if (canPrune && depth < 4 && futilityThreshold <= alpha) {
                 skipQuiet = 1;
             }
         }
