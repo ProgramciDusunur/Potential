64196c7ea7ed91ddec9695ca5abc3676d5bfeeab
ProgramciDusunur <lerenarazl@gmail.com>
2024-09-21 20:27:29 +0300

    mix with failed test and try again

--- src/search.c
+++ src/search.c
@@ -601,8 +601,8 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
                 skipQuiet = 1;
             }
             // Futility pruning
-            int futilityMargin = improving ? static_eval + 100 : static_eval + 80;
-            int futilityImprovingFactor = position->improvingRate[position->ply] * (0.225 * depth);
+            int futilityMargin = improving ? static_eval + (100 * depth) : static_eval + (80 * depth - 1);
+            int futilityImprovingFactor = position->improvingRate[position->ply] * (0.2 * depth);
             int futilityThreshold = futilityMargin + futilityImprovingFactor;
             if (canPrune && depth < 4 && futilityThreshold <= alpha) {
                 skipQuiet = 1;
