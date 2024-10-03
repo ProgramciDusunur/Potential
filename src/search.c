4a0a50327faa029b38d57bbea039e2faf8fde5bb
ProgramciDusunur <lerenarazl@gmail.com>
2024-10-03 15:52:42 +0300

    fail, worse, try same thing ----------------------------------------------------- Elo   | -0.66 +- 6.74 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -3.05 (-2.94, 2.94) [0.00, 10.00] Games | N: 6296 W: 2260 L: 2272 D: 1764 Penta | [323, 640, 1249, 598, 338] https://programcidusunur.pythonanywhere.com/test/522/ -----------------------------------------------------

--- src/search.c
+++ src/search.c
@@ -668,7 +668,7 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
                     lmrReduction += 1;
                 }*/
                 if (!pvNode && quietMoves >= 4) {
-                    lmrReduction += 1 + (moveHistory <= -2048);
+                    lmrReduction += 1 + (moveHistory <= -1024);
                 }
 
                 /*if (position->improvingRate[position->ply] < -2.0) {
