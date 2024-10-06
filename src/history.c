0f9ea76270f192bdf5c9739246a14ad4f2901240
ProgramciDusunur <lerenarazl@gmail.com>
2024-10-06 17:33:37 +0300

    failed, still bad, what about without penalty and ply 1 ---------------------------------------------------- Elo   | -23.68 +- 15.99 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -3.49 (-2.94, 2.94) [0.00, 10.00] Games | N: 1264 W: 419 L: 505 D: 340 Penta | [94, 137, 225, 113, 63] https://programcidusunur.pythonanywhere.com/test/11/ ----------------------------------------------------

--- src/history.c
+++ src/history.c
@@ -13,23 +13,23 @@ int getSingleContinuationHistoryScore(const board *position, const SearchStack *
 
 // Returns the history score of a move
 int getContinuationHistoryScore(const board *position, const SearchStack *ss, const int move) {
-    return getSingleContinuationHistoryScore(position, ss, move, 1) +
-    getSingleContinuationHistoryScore(position, ss, move, 2);
+    return getSingleContinuationHistoryScore(position, ss, move, 1);
+    //return getSingleContinuationHistoryScore(position, ss, move, 2);
     //+ GetSingleCHScore(sd, ss, move, 4);
 }
 
 void updateSingleContinuationHistoryScore(const board *position, SearchStack *ss, const int move, const int bonus, const int offSet) {
     if (position->ply >= offSet) {
         const int previousMove = (ss)->move[position->ply - offSet];
-        const int scaledBonus = bonus - getSingleContinuationHistoryScore(position, ss, move, offSet) * abs(bonus) / 32768;
+        const int scaledBonus = bonus - getSingleContinuationHistoryScore(position, ss, move, offSet) * abs(bonus) / 16384;
         ss->continuationHistory[getMoveTarget(previousMove)][getMoveTarget(move)] += scaledBonus;
     }
 }
 
 void updateContinuationHistoryScore(board *position, SearchStack *ss, const int move, const int bonus) {
-    const int scaledBonus = bonus - getContinuationHistoryScore(position, ss, move) * abs(bonus) / 16384;
+    const int scaledBonus = bonus - getContinuationHistoryScore(position, ss, move) * abs(bonus) / 8192;
     updateSingleContinuationHistoryScore(position, ss, move, scaledBonus, 1);
-    updateSingleContinuationHistoryScore(position, ss, move, scaledBonus, 2);
+    //updateSingleContinuationHistoryScore(position, ss, move, scaledBonus, 2);
     //updateSingleContinuationHistoryScore(position, ss, move, scaledBonus, 4);
 }
 
@@ -61,7 +61,7 @@ void updateHistory(board *position, SearchStack *ss, int bestMove, int depth, mo
          if (badQuiets->moves[index] == bestMove) continue;
 
          historyMoves[badQuietFrom][badQuietTo] += scaledBonus(badQuietScore, -bonus);
-         updateContinuationHistoryScore(position, ss, move, -bonus);
+         //updateContinuationHistoryScore(position, ss, move, -bonus);
      }
 }
 
