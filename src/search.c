b60713ad701ef555acc2d5ef10170bcb162649bc
ProgramciDusunur <lerenarazl@gmail.com>
2024-10-27 17:51:21 +0300

    bench: 2083236

--- src/search.c
+++ src/search.c
@@ -616,7 +616,7 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
             // Futility pruning
             int futilityHistoryFactor = ((moveHistory * 0.01) * depth);
             int futilityEvalMargin = improving ? static_eval + 100 : static_eval + 80;
-            int futilityImprovingFactor = position->improvingRate[position->ply] * (0.4 * depth);
+            int futilityImprovingFactor = position->improvingRate[position->ply] * (0.75 * depth);
             int futilityMargin = futilityEvalMargin + futilityHistoryFactor + futilityImprovingFactor;
 
             if (canPrune && depth < 4 && futilityMargin <= alpha) {
