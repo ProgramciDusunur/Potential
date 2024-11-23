023354de96e0f4eb9f17973a961c7597a42d124c
ProgramciDusunur <lerenarazl@gmail.com>
2024-11-23 16:06:48 +0300

    if we are in check then be more carefully, bench: 1831265

--- src/search.c
+++ src/search.c
@@ -639,9 +639,9 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
             if (canPrune && depth < 4 && futilityMargin <= alpha) {
                 skipQuiet = 1;
             }
-            uint8_t historyNoisyTTMoveDivisor = getMoveCapture(bestMove) ? 2 : 1;
+            uint8_t historyInCheckMultiplier = in_check ? 2 : 1;
             // Quiet History Pruning
-            if (!pvNode && depth <= 4 && moveHistory < depth * (-2048 / historyNoisyTTMoveDivisor)) {
+            if (!pvNode && depth <= 4 && moveHistory < depth * (-2048 * historyInCheckMultiplier)) {
                 skipQuiet = 1;
             }
         }
