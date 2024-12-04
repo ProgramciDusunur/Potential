823ffd207b7230429ee48570cef26fb4a2ed67c5
ProgramciDusunur <lerenarazl@gmail.com>
2024-12-05 00:36:52 +0300

    bench: 1712165

--- src/search.c
+++ src/search.c
@@ -98,7 +98,7 @@ int scoreMove(int move, board* position) {
             }
         }
 
-        int captureHistoryBonus = captureMoveHistory[position->side][getMoveSource(move)][getMoveTarget(move)] / 4;
+        int captureHistoryBonus = captureMoveHistory[position->side][getMoveSource(move)][getMoveTarget(move)] / 8;
 
         // score move by MVV LVA lookup [source piece][target piece]
         return mvvLva[getMovePiece(move)][target_piece] + 1000000000 + captureHistoryBonus;
