2d7150b04de783b752c53801ffe371a68cde5891
ProgramciDusunur <lerenarazl@gmail.com>
2024-12-05 01:02:35 +0300

    give a larger weight in quiescence for capture history, bench: 1972759

--- src/search.c
+++ src/search.c
@@ -98,7 +98,7 @@ int scoreMove(int move, board* position) {
             }
         }
 
-        int captureHistoryBonus = captureMoveHistory[position->side][getMoveSource(move)][getMoveTarget(move)] / 8;
+        int captureHistoryBonus = captureMoveHistory[position->side][getMoveSource(move)][getMoveTarget(move)] / 10;
 
         // score move by MVV LVA lookup [source piece][target piece]
         return mvvLva[getMovePiece(move)][target_piece] + 1000000000 + captureHistoryBonus;
@@ -200,7 +200,7 @@ int quiescenceScoreMove(int move, board* position) {
             }
         }
 
-        int captureHistoryBonus = captureMoveHistory[position->side][getMoveSource(move)][getMoveTarget(move)] / 8;
+        int captureHistoryBonus = captureMoveHistory[position->side][getMoveSource(move)][getMoveTarget(move)];
 
         // score move by MVV LVA lookup [source piece][target piece]
         return mvvLva[getMovePiece(move)][target_piece] + 1000000000 + captureHistoryBonus;
