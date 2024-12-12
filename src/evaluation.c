13eab32d61072975ac466289ce3b4c901a7fe40d
ProgramciDusunur <lerenarazl@gmail.com>
2024-12-12 14:04:40 +0300

    at least a yellow, soo we need a new thingy, what about pawn count? bench: 1490503 ----------------------------------------------------- Elo   | 0.81 +- 1.88 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -1.86 (-2.94, 2.94) [0.00, 5.00] Games | N: 42244 W: 10644 L: 10545 D: 21055 Penta | [538, 4588, 10757, 4715, 524] https://programcidusunur.pythonanywhere.com/test/476/ -----------------------------------------------------

--- src/evaluation.c
+++ src/evaluation.c
@@ -656,7 +656,10 @@ int evaluate(board* position) {
     // winnable
     int winnableScore = 0;
     if (game_phase == endgame) {
-        winnableScore = 6 * passedPawnCount;
+        winnableScore = 6 * passedPawnCount +
+                        8 * (countBits(position->bitboards[P]) - countBits(position->bitboards[p]))
+
+                ;
         score += winnableScore;
     }
 
