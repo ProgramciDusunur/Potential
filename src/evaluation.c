7a8166515abd777ed3cbdb832a84c3313180606f
ProgramciDusunur <lerenarazl@gmail.com>
2024-12-15 23:27:12 +0300

    try higher one, bench: 1477195

--- src/evaluation.c
+++ src/evaluation.c
@@ -660,9 +660,9 @@ int evaluate(board* position) {
         int pawnsOnBothFlanks = 0;
 
         if (!position->side && ((position->bitboards[P] & whiteKingSide) && (position->bitboards[P] & whiteQueenSide))) {
-            pawnsOnBothFlanks = 15;
+            pawnsOnBothFlanks = 20;
         } else if (position->side && ((position->bitboards[p] & blackKingSide) && (position->bitboards[p] & blackQueenSide))) {
-            pawnsOnBothFlanks = -15;
+            pawnsOnBothFlanks = -20;
         }
 
         if (!position->side && get_rank[getLS1BIndex(position->bitboards[K])] > 5) {
