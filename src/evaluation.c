a261bc691f8660372b4c0ac2f2a7901377d9fe81
ProgramciDusunur <lerenarazl@gmail.com>
2024-12-15 23:45:14 +0300

    higher one responded worse, try again with a high value anyway,  bench: 1477186

--- src/evaluation.c
+++ src/evaluation.c
@@ -660,9 +660,9 @@ int evaluate(board* position) {
         int pawnsOnBothFlanks = 0;
 
         if (!position->side && ((position->bitboards[P] & whiteKingSide) && (position->bitboards[P] & whiteQueenSide))) {
-            pawnsOnBothFlanks = 20;
+            pawnsOnBothFlanks = 25;
         } else if (position->side && ((position->bitboards[p] & blackKingSide) && (position->bitboards[p] & blackQueenSide))) {
-            pawnsOnBothFlanks = -20;
+            pawnsOnBothFlanks = -25;
         }
 
         if (!position->side && get_rank[getLS1BIndex(position->bitboards[K])] > 5) {
