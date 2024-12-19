980f20650700b6c1696941f24ef46869454f6921
ProgramciDusunur <lerenarazl@gmail.com>
2024-12-20 00:37:04 +0300

    most valuable victim and least valuable aggressor move ordering, bench: 172760

--- src/search.h
+++ src/search.h
@@ -31,7 +31,7 @@ extern int counterMoves[2][maxPly][maxPly];
 int isRepetition(board* position);
 void initializeLMRTable();
 int scoreMove(int move, board* position);
-void sort_moves(moves *moveList, int bestMove, board* position);
+void sort_moves(moves *moveList, board* position);
 void enable_pv_scoring(moves *moveList, board* position);
 void printMove(int move);
 int getLmrReduction(int depth, int moveNumber);
