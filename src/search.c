1a56db8d0f313ff231310a5286da09cfc7cbd710
ProgramciDusunur <lerenarazl@gmail.com>
2024-12-21 21:52:19 +0300

    bench: 14310411

--- src/search.c
+++ src/search.c
@@ -709,8 +709,6 @@ int negamax(int alpha, int beta, int depth, board* position, time* time) {
     return alpha;
 }
 
-
-
 // search position for the best move
 void searchPosition(int depth, board* position, bool benchmark, time* time) {
     // define best score variable
