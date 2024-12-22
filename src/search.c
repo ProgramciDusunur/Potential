fe5cfe64a7f2f8ebc7dbefd183932dd02a67a880
ProgramciDusunur <lerenarazl@gmail.com>
2024-12-22 13:53:08 +0300

    try higher margin

--- src/search.c
+++ src/search.c
@@ -523,7 +523,7 @@ int negamax(int alpha, int beta, int depth, board* position, time* time) {
     bool canPrune = in_check == 0 && pvNode == 0;
 
     // reverse futility pruning
-    if (depth <= 4 && canPrune && staticEval - 100 * depth >= beta)
+    if (depth <= 4 && canPrune && staticEval - 150 * depth >= beta)
         return staticEval;
 
     // null move pruning
