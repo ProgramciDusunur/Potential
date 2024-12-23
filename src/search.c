58ba678a7ea5c5dfa3cdcc2e28818a89e601b5ec
ProgramciDusunur <lerenarazl@gmail.com>
2024-12-23 21:12:10 +0300

    bench: 10518524

--- src/search.c
+++ src/search.c
@@ -632,7 +632,6 @@ int negamax(int alpha, int beta, int depth, board* position, time* time) {
         position->repetitionIndex++;
         position->repetitionTable[position->repetitionIndex] = position->hashKey;
 
-
         // make sure to make only legal moves
         if (makeMove(moveList->moves[count], allMoves, position) == 0) {
             // decrement ply
