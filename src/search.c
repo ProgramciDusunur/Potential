d6ef7d429bca048d6b1225934860567f2bb52b1c
ProgramciDusunur <lerenarazl@gmail.com>
2024-12-25 13:39:48 +0300

    bench: 8687758

--- src/search.c
+++ src/search.c
@@ -530,7 +530,7 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
         return static_eval;
 
     // null move pruning
-    if (depth >= nullMoveDepth && in_check == 0 && position->ply) {
+    if (depth >= nullMoveDepth && in_check == 0 && position->ply && static_eval >= beta) {
         struct copyposition copyPosition;
         // preserve board state
         copyBoard(position, &copyPosition);
