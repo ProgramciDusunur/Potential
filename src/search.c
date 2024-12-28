0963325bc4baead8eea1d07f59f6c998f864c2b0
ProgramciDusunur <lerenarazl@gmail.com>
2024-12-28 19:32:10 +0300

    bench: 9675317

--- src/search.c
+++ src/search.c
@@ -707,7 +707,6 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
                     lmrReduction += 1;
                 }
 
-
             } else {
 
             }
