214b891c30d6cbfd0ae1354762d9d584709406e7
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-01 20:51:03 +0300

    bench: 9582102

--- src/search.c
+++ src/search.c
@@ -867,7 +867,7 @@ void searchPosition(int depth, board* position, bool benchmark, time* time) {
             time->stopped = 1;
         }
 
-        int window = 9;
+        int window = 27;
 
         int failHighCount = 0;
         while (true) {
