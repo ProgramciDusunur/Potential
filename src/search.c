5c4796fee22303fd06d42c94e0dfd2a97031c0c4
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-02 14:46:03 +0300

    bench: 9799415

--- src/search.c
+++ src/search.c
@@ -905,7 +905,6 @@ void searchPosition(int depth, board* position, bool benchmark, time* time) {
 
         }
 
-
         int endTime = getTimeMiliSecond();
         totalTime += endTime - startTime;
 
