76107c4fe37312ba9d5f3b59044eb6357e0ce229
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-04 01:37:44 +0300

    do more razoring

--- src/search.c
+++ src/search.c
@@ -588,7 +588,7 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
 
 
     // razoring
-    if (canPrune && depth <= 3 && static_eval + 200 * depth < alpha) {
+    if (canPrune && depth <= 4 && static_eval + 200 * depth < alpha) {
         int razoringScore = quiescence(alpha, beta, position, time);
         if (razoringScore <= alpha) {
             return razoringScore;
