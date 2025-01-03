b56336f38f989d39b01a5be1225dbb3c8180262d
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-04 00:30:22 +0300

    bench: 12373180

--- src/search.c
+++ src/search.c
@@ -642,6 +642,7 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
                 skipQuiet = 1;
             }
 
+
             int futilityMargin = (static_eval + 82 * depth) + ((moveHistory / 500) * depth);
 
             if (canPrune && depth <= 2 && futilityMargin <= alpha) {
