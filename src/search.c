a8ce01c5432b072e401e5bb391057d67d6bdfd2a
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-04 04:29:25 +0300

    if we are improving then do less late move pruning

--- src/search.c
+++ src/search.c
@@ -634,7 +634,7 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
 
             int lmpBase = 4;
             int lmpMultiplier = 3;
-            int lmpThreshold = (lmpBase + lmpMultiplier * (depth - 1) * (depth - 1));
+            int lmpThreshold = (lmpBase + (lmpMultiplier + improving) * (depth - 1) * (depth - 1));
 
             if (legal_moves>= lmpThreshold) {
                 skipQuiet = 1;
