e5b6f7e95cf73429fe7a77bfb8d1c0aad5e82500
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-04 04:29:37 +0300

    bench: 14347534

--- src/search.c
+++ src/search.c
@@ -640,6 +640,8 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
                 skipQuiet = 1;
             }
 
+
+
             if (canPrune && depth <= 2 && static_eval + 82 * depth <= alpha) {
                 skipQuiet = 1;
             }
