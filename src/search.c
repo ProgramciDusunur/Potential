81ef593064a99661afec02900012ae8985f56a47
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-05 12:41:54 +0300

    bench: 10515923

--- src/search.c
+++ src/search.c
@@ -695,7 +695,6 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
             /* All Moves */
 
             // Reduce More
-
             if (!improving) {
                 lmrReduction += 1;
             }
