6620e2f24e9d00ccedae78bec22b84b9e15cc428
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-13 22:17:14 +0300

    change formula

--- src/search.c
+++ src/search.c
@@ -637,7 +637,8 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
 
             int lmpBase = 4;
             int lmpMultiplier = 3;
-            int lmpThreshold = (lmpBase + lmpMultiplier * (depth - 1) * (depth - 1)) + ((moveHistory / 2000) * depth);
+
+            int lmpThreshold = (lmpBase + lmpMultiplier * (depth - 1) * (depth - 1)) + ((moveHistory * 0.001) * depth);
 
             if (legal_moves>= lmpThreshold) {
                 skipQuiet = 1;
