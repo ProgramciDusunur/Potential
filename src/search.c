1c2d91c5bf54be03dfb0639f79864548f0af4e10
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-19 18:51:24 +0300

    continue ply check to prevent incorrect memory accesses

--- src/search.c
+++ src/search.c
@@ -713,7 +713,7 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
         pastStack = position->ply - 4;
     }
 
-    if (pastStack && !in_check) {
+    if (pastStack && position->ply >= pastStack && !in_check) {
         improving = position->staticEval[position->ply] > position->staticEval[pastStack];
         const double diff = position->staticEval[position->ply] - position->staticEval[pastStack];
         position->improvingRate[position->ply] = fmin(fmax(position->improvingRate[position->ply] + diff / 50, (-1.0)), 1.0);
