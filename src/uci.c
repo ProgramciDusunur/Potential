de6bc3c6676a336d5a0c4a939a675f7ead0116c0
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-20 22:47:21 +0300

    bench: 8153733

--- src/uci.c
+++ src/uci.c
@@ -143,7 +143,7 @@ void parse_position(char *command, board* position) {
 }
 
 void scaleTime(time* time, uint8_t bestMoveStability) {
-    double bestMoveScale[6] = {2.43, 1.35, 1.09, 0.88, 0.68, 0.56};
+    double bestMoveScale[5] = {1.944, 1.08, 0.872, 0.704, 0.544};
     time->softLimit =
             myMIN(time->starttime + time->baseSoft * bestMoveScale[bestMoveStability], time->maxTime + time->starttime);
 }
