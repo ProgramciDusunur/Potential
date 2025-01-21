741ab5654daab0ffeee1d3007b62b6b603cf33e8
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-21 17:36:11 +0300

    bench: 8075523

--- src/search.c
+++ src/search.c
@@ -650,11 +650,6 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
             return alpha;
     }
 
-    //int ttBound = readHashFlag(position);
-
-    //bool improving;
-
-    //int pastStack;
 
     // read hash entry
     if (!rootNode &&
