52be4db00fea450d1eadf56a917ac915937f4b2f
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-24 23:48:43 +0300

    bench: 8153733

--- src/time.c
+++ src/time.c
@@ -48,37 +48,41 @@ int getTimeMiliSecond(void) {
     }
 
 int input_waiting(void) {
-    #ifndef WIN32
-        fd_set readfds;
-            struct timeval tv;
-            FD_ZERO (&readfds);
-            FD_SET (fileno(stdin), &readfds);
-            tv.tv_sec=0; tv.tv_usec=0;
-            select(16, &readfds, 0, 0, &tv);
+#ifndef _WIN32
+    fd_set readfds;
+  struct timeval tv;
+  FD_ZERO(&readfds);
+  FD_SET(fileno(stdin), &readfds);
+  tv.tv_sec = 0;
+  tv.tv_usec = 0;
+  select(16, &readfds, 0, 0, &tv);
 
-            return (FD_ISSET(fileno(stdin), &readfds));
-    #else
-        int init = 0, pipe;
-        HANDLE inh;
-        DWORD dw;
+  return (FD_ISSET(fileno(stdin), &readfds));
+#else
+    static int init = 0, pipe;
+    static HANDLE inh;
+    DWORD dw;
 
-        if (!init) {
-            init = 1;
-            inh = GetStdHandle(STD_INPUT_HANDLE);
-            pipe = !GetConsoleMode(inh, &dw);
-            if (!pipe) {
-                SetConsoleMode(inh, dw & ~(ENABLE_MOUSE_INPUT | ENABLE_WINDOW_INPUT));
-                FlushConsoleInputBuffer(inh);
-            }
+    if (!init) {
+        init = 1;
+        inh = GetStdHandle(STD_INPUT_HANDLE);
+        pipe = !GetConsoleMode(inh, &dw);
+        if (!pipe) {
+            SetConsoleMode(inh, dw & ~(ENABLE_MOUSE_INPUT | ENABLE_WINDOW_INPUT));
+            FlushConsoleInputBuffer(inh);
         }
+    }
 
-        if (pipe) {
-            if (!PeekNamedPipe(inh, NULL, 0, NULL, &dw, NULL)) return 1;
-            return dw;
-        } else {
-            GetNumberOfConsoleInputEvents(inh, &dw);
-            return dw <= 1 ? 0 : dw;
-        }
+    if (pipe) {
+        if (!PeekNamedPipe(inh, NULL, 0, NULL, &dw, NULL))
+            return 1;
+        return dw;
+    }
 
-    #endif
+    else {
+        GetNumberOfConsoleInputEvents(inh, &dw);
+        return dw <= 1 ? 0 : dw;
+    }
+
+#endif
 }
