4757bcbb6b02cdc9d16d833db132e44c8d3de8c0
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-01 01:27:21 +0300

    bench: 17712741

--- src/search.c
+++ src/search.c
@@ -571,7 +571,6 @@ int negamax(int alpha, int beta, int depth, board* position, time* time) {
     }
 
 
-
     // create move list instance
     moves moveList[1], badQuiets[1];
     badQuiets->count = 0;
