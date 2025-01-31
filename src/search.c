d9c717493466334d0a469ca38ab230762eb37049
ProgramciDusunur <lerenarazl@gmail.com>
2025-01-31 03:34:30 +0300

    bench: 5587585

--- src/search.c
+++ src/search.c
@@ -542,7 +542,6 @@ int negamax(int alpha, int beta, int depth, board* position, time* time) {
 
 
 
-
         // do normal alpha beta search
         score = -negamax(-beta, -alpha, depth - 1, position, time);
 
