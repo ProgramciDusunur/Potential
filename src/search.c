86c4eb4d5f7d4c0a7a3e74da8e957793f5f6ea5e
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-01 17:51:12 +0300

    bench: 10784413

--- src/search.c
+++ src/search.c
@@ -450,6 +450,7 @@ int negamax(int alpha, int beta, int depth, board* position, time* time) {
         }
     }
 
+
     // recursion escapre condition
     if (depth == 0)
         // run quiescence search
