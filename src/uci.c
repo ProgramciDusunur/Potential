5f915dfe07880a463d6d52438bb9057f62d1bacd
Eren Araz <77514455+ProgramciDusunur@users.noreply.github.com>
2025-02-02 02:00:17 +0300

    Lmr table, STC pass! (#194)
    * add lmr table
    
    * bench: 13161742

--- src/uci.c
+++ src/uci.c
@@ -369,7 +369,7 @@ void uciProtocol(int argc, char *argv[]) {
 
     if (argc >= 2 && strncmp(argv[1], "bench", 5) == 0) {
         printf("bench running..\n");
-        benchmark(10, &position, &time);
+        benchmark(11, &position, &time);
         return;
     }
 
@@ -484,8 +484,7 @@ void uciProtocol(int argc, char *argv[]) {
             printf("uciok\n");
         }
         else if (strncmp(input, "bench", 5) == 0) {
-            benchmark(10, &position, &time);
+            benchmark(11, &position, &time);
         }
     }
 }
-
---
+++ src/utils.c
@@ -0,0 +1,14 @@
+//
+// Created by erena on 2.02.2025.
+//
+
+#include "utils.h"
+
+
+int myMAX(int x, int y) {
+    return (x > y) ? x : y;
+}
+
+int myMIN(int x, int y) {
+    return (x < y) ? x : y;
+}
