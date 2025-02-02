8abf2b594f6597d8891f9d435d6a100a66658bdd
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-02 03:07:11 +0300

    bench: 8629236

--- src/search.c
+++ src/search.c
@@ -474,31 +474,7 @@ int negamax(int alpha, int beta, int depth, board* position, time* time) {
     // get static evaluation score
     int static_eval = evaluate(position);
 
-    /*position->staticEval[position->ply] = static_eval;
 
-    position->improvingRate[position->ply] = 0.0;
-
-    if (position->staticEval[position->ply-2] != noEval) {
-        pastStack = position->ply - 2;
-    } else if (position->staticEval[position->ply-4] != noEval) {
-        pastStack = position->ply - 4;
-    }
-
-    if (pastStack) {
-        const double diff = position->staticEval[position->ply] - position->staticEval[pastStack];
-        position->improvingRate[position->ply] = fmin(fmax(position->improvingRate[position->ply] + diff / 50, -3.0), 2.0);
-    }*/
-
-    /*if(in_check)
-        improving = false;
-    else if (position->staticEval[position->ply-2] != noEval) {
-        improving = position->staticEval[position->ply] > position->staticEval[position->ply-2];
-    }
-    else if (position->staticEval[position->ply-4] != noEval) {
-        improving = position->staticEval[position->ply] > position->staticEval[position->ply-4];
-    }
-    else
-        improving = true;*/
 
 
     bool canPrune = in_check == 0 && pvNode == 0;
