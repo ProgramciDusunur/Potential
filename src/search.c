4299f91c67c46ef2a9cc9142ba533eaa45ecb512
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-03 02:51:03 +0300

    bench: 6129974

--- src/search.c
+++ src/search.c
@@ -800,7 +800,6 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
                         updateQuietMoveHistory(bestMove, depth, badQuiets);
                     }
 
-
                     // node (move) fails high
                     break;
                 }
