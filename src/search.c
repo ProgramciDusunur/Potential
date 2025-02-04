c8115ebeab058cba4cb75757bdb110200cf39f33
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-04 04:26:21 +0300

    if we are improving than reduce more

--- src/search.c
+++ src/search.c
@@ -708,6 +708,13 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
         } else {
             int lmrReduction = getLmrReduction(depth, legal_moves);
 
+            /* All Moves */
+
+            // Reduce More
+            if (!improving) {
+                lmrReduction += 1;
+            }
+
             if (isQuiet) {
 
                 // Reduce More
