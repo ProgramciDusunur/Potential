144c0850253eaa2d0c3af1d0eda1606fe38120c3
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-06 20:26:28 +0300

    Elo   | 1.77 +- 4.17 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -2.96 (-2.94, 2.94) [0.00, 10.00] Games | N: 13968 W: 4116 L: 4045 D: 5807 Penta | [490, 1651, 2646, 1692, 505] https://programcidusunur.pythonanywhere.com/test/1105/
    Elo   | 1.75 +- 3.70 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=8MB
    LLR   | -2.96 (-2.94, 2.94) [0.00, 10.00]
    Games | N: 15080 W: 4509 L: 4433 D: 6138
    Penta | [429, 1599, 3404, 1683, 425]
    https://programcidusunur.pythonanywhere.com/test/1126/
    
    ANOTHER ARMAGEDDON! Let's go!

--- src/search.c
+++ src/search.c
@@ -869,12 +869,21 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
                 lmrReduction += 1;
             }
 
+            if (cutNode) {
+                lmrReduction += 1;
+            }
+
 
             if (isQuiet) {
 
                 // Reduce More
                 if (!pvNode && quietMoves >= 4) {
                     lmrReduction += 1;
+                }
+
+                // Reduce Less
+                if (in_check) {
+                    lmrReduction -= 1;
                 }
 
             }
