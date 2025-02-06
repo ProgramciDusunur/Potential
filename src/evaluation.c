d570c3dea44b08f46e50b2232f4f9af095a52450
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-06 13:16:26 +0300

    bench: 5736880

--- src/evaluation.c
+++ src/evaluation.c
@@ -534,7 +534,6 @@ int evaluate(board* position) {
                         score -= passed_pawn_bonus_middle[mirrorScore[square]];
                     }
 
-
                     break;
 
                     // evaluate black knights
