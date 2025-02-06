cf22d9b760110f0b69a3c236a8fda5bc478aa738
Eren Araz <77514455+ProgramciDusunur@users.noreply.github.com>
2025-02-06 23:14:04 +0300

    Eval TM, STC pass! (#220)
    * add evaluation based time management
    
    * bench: 5793382

--- src/search.c
+++ src/search.c
@@ -1031,6 +1031,8 @@ void searchPosition(int depth, board* position, bool benchmark, time* time) {
 
     int previousBestMove = 0;
     uint8_t bestMoveStability = 0;
+    int averageScore = noEval;
+    uint8_t evalStability = 0;
 
     // iterative deepening
     for (int current_depth = 1; current_depth <= depth; current_depth++) {
@@ -1088,14 +1090,25 @@ void searchPosition(int depth, board* position, bool benchmark, time* time) {
 
         }
 
+        averageScore = averageScore == noEval ? score : (averageScore + score) / 2;
+
+
         if (position->pvTable[0][0] == previousBestMove) {
             bestMoveStability = myMIN(bestMoveStability + 1, 4);
         } else {
             previousBestMove = position->pvTable[0][0];
             bestMoveStability = 0;
         }
+
+        if (score > averageScore - 10 && score < averageScore + 10) {
+            evalStability = myMIN(evalStability + 1, 4);
+        } else {
+            evalStability = 0;
+        }
+
+
         if (time->timeset && current_depth > 6) {
-            scaleTime(time, bestMoveStability);
+            scaleTime(time, bestMoveStability, evalStability);
         }
 
         int endTime = getTimeMiliSecond();
