c82864cae5399e0a5350a2ce1e50f14f3291b54b
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-08 01:07:17 +0300

    bench: 592928

--- src/search.c
+++ src/search.c
@@ -726,7 +726,6 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
     if (depth <= 5 && !pvNode && !in_check && static_eval - rfpMargin >= beta)
         return (static_eval + beta) / 2;
 
-
     // null move pruning
     if (depth >= nullMoveDepth && in_check == 0 && !rootNode &&
             static_eval >= beta &&
