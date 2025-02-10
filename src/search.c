aab839b3a0c3e08ff1bf3f8e3261658dcaf94054
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-10 12:57:56 +0300

    if the king in a danger then reduce less

--- src/search.c
+++ src/search.c
@@ -919,6 +919,11 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
                 lmrReduction += 1;
             }
 
+            // Reduce Less
+            if (in_check) {
+                lmrReduction -= 1;
+            }
+
 
             if (isQuiet) {
 
