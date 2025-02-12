11093ac15922189675a8aa0fcdb0fd0c60799116
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-12 14:29:16 +0300

    bench: 5717978

--- src/search.c
+++ src/search.c
@@ -743,7 +743,6 @@ int negamax(int alpha, int beta, int depth, board* position, time* time, bool cu
         position->improvingRate[position->ply] = fmin(fmax(position->improvingRate[position->ply] + diff / 50, (-1.0)), 1.0);
     }
 
-
     // Internal Iterative Reductions
     if ((pvNode || cutNode || !improving) && depth >= 8 && !tt_move) {
         depth -= 1 + cutNode;
