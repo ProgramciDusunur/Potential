d2392315c3b36e11d6d5e78794b89d6c79e6737e
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-13 04:18:18 +0300

    bench: 7419967

--- src/search.c
+++ src/search.c
@@ -939,26 +939,29 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             const int singularBeta = tt_score - depth * 2;
             const int singularDepth = (depth - 1) / 2;
 
+            pos->isSingularMove[pos->ply] = currentMove;
 
             // decrement ply
             pos->ply--;
 
+            // decrement repetition index
+            pos->repetitionIndex--;
 
             // take move back
             takeBack(pos, &copyPosition);
 
-            pos->isSingularMove[pos->ply] = currentMove;
-
             const int singularScore =
                     negamax(singularBeta - 1, singularBeta, singularDepth, pos, time, cutNode);
 
+            pos->isSingularMove[pos->ply] = 0;
+
             makeMove(moveList->moves[count], allMoves, pos);
 
-            // increment ply
             pos->ply++;
 
-
-            pos->isSingularMove[pos->ply] = 0;
+            // increment repetition index & store hash key
+            pos->repetitionIndex++;
+            pos->repetitionTable[pos->repetitionIndex] = pos->hashKey;
 
             if (singularScore < singularBeta) {
                 extensions++;
