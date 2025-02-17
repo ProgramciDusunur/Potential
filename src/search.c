81b4a364260e8ae3ed6f31fae10a61c9d2866037
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-17 05:51:17 +0300

    try multicut

--- src/search.c
+++ src/search.c
@@ -1004,6 +1004,14 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
                 }
 
             }
+
+            // Multicut: Singular search failed high so if singular beta beats our
+            // beta we can assume the main search will also fail high and thus we can
+            // just cutoff here
+            else if (singularBeta >= beta) {
+                return singularBeta;
+            }
+
             // Negative Extension
             else if (tt_score >= beta) {
                 extensions--;
