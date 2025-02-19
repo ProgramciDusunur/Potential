4f328e8f63b9c0ebd3ca35733a2cf5bce3893e09
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-19 15:51:24 +0300

    bench: 8366618

--- src/search.c
+++ src/search.c
@@ -1058,7 +1058,6 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
                 lmrReduction += 1;
             }
 
-
             if (isQuiet) {
 
                 // Reduce More
