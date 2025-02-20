527b1de5990fb3e6aa9672aa3377cb3b3087b515
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-21 00:43:46 +0300

    bench: 8404120

--- src/search.c
+++ src/search.c
@@ -1018,7 +1018,6 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             else if (tt_score >= beta) {
                 extensions -= 1 + !pvNode + cutNode;
             }
-
         }
 
         // increment nodes count
