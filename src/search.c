b3a1dc9634401730375ec4406e9de487c63df2f2
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-21 13:26:50 +0300

    bench: 8101251

--- src/search.c
+++ src/search.c
@@ -802,7 +802,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
 
     // reverse futility pruning
     if (!pos->isSingularMove[pos->ply] &&
-        depth <= 5 && !pvNode && !in_check && tt_move == 0 && static_eval - rfpMargin >= beta)
+        depth <= 5 && !pvNode && !in_check && !tt_hit && static_eval - rfpMargin >= beta)
         return static_eval;
 
     // null move pruning
