359314266a9febf0045b64f06e757c0471304ae3
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-21 16:03:19 +0300

    bench: 7904651

--- src/search.c
+++ src/search.c
@@ -1014,6 +1014,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
                 }
 
             }
+
             // Multicut
             else if (singularBeta >= beta) {
                 // decrement ply
