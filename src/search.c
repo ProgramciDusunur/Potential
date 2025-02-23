c8d2199d40b4f88bdc20b2612a884851a5e37573
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-23 20:03:17 +0300

    bench: 7860302

--- src/search.c
+++ src/search.c
@@ -753,6 +753,8 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         }
     }
 
+    score = tt_score;
+
     // recursion escapre condition
     if (depth <= 0)
         // run quiescence search
@@ -765,12 +767,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
                                     pos->side ^ 1, pos);
 
 
-    int raw_eval;
-    if (!pos->isSingularMove[pos->ply]) {
-        raw_eval = in_check ? noScore : (tt_hit ? tt_score : evaluate(pos));
-    } else {
-        raw_eval = evaluate(pos);
-    }
+    int raw_eval = evaluate(pos);
 
 
     int static_eval = adjustEvalWithCorrectionHistory(pos, raw_eval);
