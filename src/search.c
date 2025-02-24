c8812a3b47bbc1ba4f11b82d4a68730c661de80d
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-24 17:08:30 +0300

    bench: 8067921

--- src/search.c
+++ src/search.c
@@ -809,7 +809,6 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
 
     uint16_t rfpMargin = improving ? 65 * (depth - 1) : 82 * depth;
 
-
     // reverse futility pruning
     if (!pos->isSingularMove[pos->ply] &&
         depth <= 5 && !pvNode && !in_check && !tt_hit && static_eval - rfpMargin >= beta)
