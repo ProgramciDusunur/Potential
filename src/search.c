f5ebf18b7387c00c2edcb9bf8afb907bd566bb4a
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-27 20:55:12 +0300

    bench: 6134514

--- src/search.c
+++ src/search.c
@@ -1038,7 +1038,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
 
 
             // Corrplexity Extension
-            else if (abs(static_eval - raw_eval) < 2 * depth) {
+            else if (abs(static_eval - raw_eval) < 8 * depth) {
                 extensions -= 1;
             }
         }
