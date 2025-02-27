df43005d0f85c7552b3674c903499aa8e7f8de17
ProgramciDusunur <lerenarazl@gmail.com>
2025-02-27 04:05:43 +0300

    if we are expect a cut node then extend less

--- src/search.c
+++ src/search.c
@@ -1035,6 +1035,11 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             else if (tt_score >= beta) {
                 extensions -= 1 + !pvNode;
             }
+
+            // Cut Node Extension
+            else if (cutNode) {
+                extensions -= 1;
+            }
         }
 
         // increment nodes count
