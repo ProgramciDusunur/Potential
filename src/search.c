dd02aea1ef196baed2d17d8ccd0ee808d0c81cfa
ProgramciDusunur <lerenarazl@gmail.com>
2025-03-02 02:46:18 +0300

    bench: 7271938

--- src/search.c
+++ src/search.c
@@ -1078,12 +1078,10 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
 
 
         if (isQuiet) {
-
             // Reduce More
             if (!pvNode && quietMoves >= 4) {
                 lmrReduction += 1;
             }
-
         }
 
         lmrReduction = clamp(lmrReduction, 1, new_depth);
