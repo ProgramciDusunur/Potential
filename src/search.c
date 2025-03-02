67f170d1986224016d646c82c4ed24e47d18aa2c
ProgramciDusunur <lerenarazl@gmail.com>
2025-03-02 23:58:20 +0300

    bench: 7925489

--- src/search.c
+++ src/search.c
@@ -1040,7 +1040,6 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
                 }
 
             }
-
             // Negative Extension
             else if (tt_score >= beta) {
                 extensions -= 1 + !pvNode;
