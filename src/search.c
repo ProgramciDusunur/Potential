769debdd5a6b2c3fcebda1d713f0f767a8492bd5
ProgramciDusunur <lerenarazl@gmail.com>
2025-03-02 03:12:24 +0300

    bench: 6478615

--- src/search.c
+++ src/search.c
@@ -1092,7 +1092,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
 
             if (score > alpha && lmrReduction != 0) {
                 bool doDeeper = score > bestScore + 35 + 2 * new_depth;
-                bool doShallower = score < (bestScore + new_depth);
+                bool doShallower = score < bestScore + 2 * (depth / 2);
                 new_depth += doDeeper - doShallower;
                 score = -negamax(-alpha - 1, -alpha, new_depth, pos, time, !cutNode);
             }
