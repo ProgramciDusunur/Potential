dbd81e6d12cb2bd920e5f2972a18a93ecb900bec
ProgramciDusunur <lerenarazl@gmail.com>
2025-03-05 15:39:55 +0300

    bench: 4905457

--- src/search.c
+++ src/search.c
@@ -1052,6 +1052,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
 
         /* All Moves */
 
+
         // Reduce More
         if (!improving) {
             lmrReduction += 1;
