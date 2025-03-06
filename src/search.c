a1373d6be23203560747324abbebbdefec5baf7f
Eren Araz <lerenarazl@gmail.com>
2025-03-07 00:18:14 +0300

    bench: 5617301

--- src/search.c
+++ src/search.c
@@ -588,7 +588,6 @@ int quiescence(int alpha, int beta, board* position, time* time) {
         return evaluation;
     }
 
-
     // found a better move
     if (evaluation > alpha) {
         // PV node (move)
