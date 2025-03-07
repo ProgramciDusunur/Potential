c5292a227fd8d993a49cdc3a7770237d043489c9
Eren Araz <lerenarazl@gmail.com>
2025-03-07 20:53:43 +0300

    try deeper and shallower LMR search

--- src/search.c
+++ src/search.c
@@ -1080,6 +1080,9 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             score = -negamax(-alpha - 1, -alpha, reduced_depth, pos, time, true);
 
             if (score > alpha && lmrReduction != 0) {
+                bool doDeeper = score > bestScore + 35 + 2 * new_depth;
+                bool doShallower = score < bestScore + 2 * (depth / 2);
+                new_depth += doDeeper - doShallower;
                 score = -negamax(-alpha - 1, -alpha, new_depth, pos, time, !cutNode);
             }
         }
