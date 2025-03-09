590d9e4a9d0f602ebd19ef76cba43f4bbbeacb0a
Eren Araz <lerenarazl@gmail.com>
2025-03-09 17:01:04 +0300

    bench: 5319101

--- src/evaluation.c
+++ src/evaluation.c
@@ -242,7 +242,7 @@ const int queen_unit = 9;
 // Mobility Bonuses
 const int bishop_mobility_middlegame = 5;
 const int bishop_mobility_endgame = 10;
-const int queen_mobility_middlegame = 1;
+const int queen_mobility_middlegame = 4;
 const int queen_mobility_endgame = 2;
 
 // King's Bonuses
