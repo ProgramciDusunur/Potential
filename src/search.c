92770bed51fdd7f2aa7b938f968298bb4c85e68c
Eren Araz <lerenarazl@gmail.com>
2025-03-11 02:05:11 +0300

    bench: 4834858

--- src/search.c
+++ src/search.c
@@ -1037,6 +1037,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             else if (tt_score >= beta) {
                 extensions -= 1 + !pvNode;
             }
+
         }
 
         // increment nodes count
