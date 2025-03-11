9b7e01c9ca838e10d6fff39bed0dac8eeba78f28
Eren Araz <lerenarazl@gmail.com>
2025-03-11 20:24:50 +0300

    bench: 4931777

--- src/search.c
+++ src/search.c
@@ -1026,7 +1026,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             }
 
             // Multicut
-            else if (singularBeta >= beta + 30 - (myMIN(2 * depth, 30))) {
+            else if (singularBeta >= beta + 20 - (myMIN(2 * depth, 10))) {
                 // decrement ply
                 pos->ply--;
 
