1776cc6169cc4e649aa73868716792cc64848a81
Eren Araz <lerenarazl@gmail.com>
2025-03-12 22:53:25 +0300

    refactor is quiet control

--- src/search.c
+++ src/search.c
@@ -910,7 +910,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             continue;
         }
 
-        bool isQuiet = getMoveCapture(currentMove) == 0;
+        bool isQuiet = getMoveCapture(currentMove) == 0 && getMovePromoted(currentMove) == 0;
 
         //bool isMoveTactical = isTactical(currentMove);
 
