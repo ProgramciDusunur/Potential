da2a1891c6ef07df1ded6dd4bce90ba7c427c931
Eren Araz <lerenarazl@gmail.com>
2025-03-13 19:09:19 +0300

    bench: 5523640

--- src/search.c
+++ src/search.c
@@ -977,7 +977,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         // A rather simple idea that if our TT move is accurate we run a reduced
         // search to see if we can beat this score. If not we extend the TT move
         // search
-        if (!rootNode && depth >= 7 + tt_pv && currentMove == tt_move && !pos->isSingularMove[pos->ply] &&
+        if (!rootNode && depth >= 7 + pvNode && currentMove == tt_move && !pos->isSingularMove[pos->ply] &&
             tt_depth >= depth - 3 && tt_flag != hashFlagBeta &&
             abs(tt_score) < mateScore) {
             const int singularBeta = tt_score - depth;
