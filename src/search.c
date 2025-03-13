3024f7b983decf536edca890ef65eeaec16ad7fe
Eren Araz <lerenarazl@gmail.com>
2025-03-14 01:14:31 +0300

    bench: 4632038

--- src/search.c
+++ src/search.c
@@ -1049,9 +1049,6 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         /* All Moves */
 
         // Reduce More
-        if (!improving) {
-            lmrReduction += 1;
-        }
 
         if (isQuiet) {
             // Reduce More
@@ -1067,6 +1064,10 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         // Reduce Less
         if (tt_pv) {
             lmrReduction -= 1;
+        }
+
+        if (!improving) {
+            lmrReduction += 1;
         }
 
         */
