21f8643febddcd58b96caec681e60bad357d9ee1
Eren Araz <lerenarazl@gmail.com>
2025-03-13 18:51:05 +0300

    rework with tt pv LMR

--- src/search.c
+++ src/search.c
@@ -1058,14 +1058,15 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             }
         }
 
-        /*if (!improving) {
-            lmrReduction += 1;
-        }
-
         // Reduce Less
         if (tt_pv) {
             lmrReduction -= 1;
         }
+
+        /*if (!improving) {
+            lmrReduction += 1;
+        }
+
 
         */
 
