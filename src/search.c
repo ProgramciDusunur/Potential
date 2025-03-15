cd84e926db59f0a20fbe6bad0ca84bf34fd50b43
Eren Araz <lerenarazl@gmail.com>
2025-03-15 17:51:17 +0300

    tweak quiet history pruning

--- src/search.c
+++ src/search.c
@@ -1159,7 +1159,8 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
                     if (notTactical) {
                         // store killer moves
                         pos->killerMoves[pos->ply][0] = bestMove;
-                        updateQuietMoveHistory(bestMove, pos->side, depth, badQuiets);
+                        int historyDepth = depth + (bestScore > beta + 50);
+                        updateQuietMoveHistory(bestMove, pos->side, historyDepth, badQuiets);
 
                         if (rootNode) {
                             updateRootHistory(pos, bestMove, depth, badQuiets);
