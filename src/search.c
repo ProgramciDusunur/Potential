a6a2c59d53cf0dc43d8bc9ffa8afd01f28de20f8
Eren Araz <lerenarazl@gmail.com>
2025-03-17 15:36:47 +0300

    bench: 5242216

--- src/search.c
+++ src/search.c
@@ -1091,7 +1091,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             score = -negamax(-alpha - 1, -alpha, reduced_depth, pos, time, true);
 
             if (score > alpha && lmrReduction != 0) {
-                bool doDeeper = score > bestScore + 70;
+                bool doDeeper = score > bestScore + 35;
                 new_depth += doDeeper;
                 score = -negamax(-alpha - 1, -alpha, new_depth, pos, time, !cutNode);
             }
