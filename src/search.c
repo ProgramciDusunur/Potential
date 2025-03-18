8750e5971a13130b0e01bf92e663e6f4cc7b1c50
Eren Araz <lerenarazl@gmail.com>
2025-03-18 18:13:25 +0300

    add multicut

--- src/search.c
+++ src/search.c
@@ -1036,6 +1036,18 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
 
             }
 
+            // Multicut
+            else if (singularBeta >= beta) {
+                // decrement ply
+                pos->ply--;
+
+                // decrement repetition index
+                pos->repetitionIndex--;
+
+                return singularBeta;
+            }
+
+
             // Negative Extension
             else if (tt_score >= beta) {
                 extensions -= 1 + !pvNode;
