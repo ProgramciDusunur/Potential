ab771ff65e516c6410aad733f755552efad93cad
Eren Araz <lerenarazl@gmail.com>
2025-03-20 18:09:00 +0300

    bench: 6184335

--- src/evaluation.c
+++ src/evaluation.c
@@ -252,7 +252,7 @@ const int queen_mobility_endgame = 2;
 // King's Bonuses
 const int king_shield_bonus_middlegame = 6;
 const int king_shield_bonus_endgame = 2;
-const int king_distance_bonus = 2;
+const int king_distance_bonus = 6;
 
 // Game Phase Scores
 const int opening_phase_score = 7740;
