2478dc477c840a50eb279634ec9273525f519c19
Eren Araz <lerenarazl@gmail.com>
2025-03-23 18:44:30 +0300

    bench: 5757963

--- src/search.c
+++ src/search.c
@@ -1074,9 +1074,10 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
 
             }
 
-            // Negative Extension
+            // Negative Extensions
             else if (tt_score >= beta) {
                 extensions -= 1 + !pvNode;
+                // Double Negative Extension
                 if (!pvNode && tt_score >= beta + 20) {
                     extensions -= 1;
                 }
