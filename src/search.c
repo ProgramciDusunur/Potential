35a7baa15c07e3318063ddec06fb81aa5fbee3a7
Eren Araz <lerenarazl@gmail.com>
2025-03-25 02:08:20 +0300

    bench: 6329625

--- src/search.c
+++ src/search.c
@@ -929,7 +929,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
     int legal_moves = 0;
 
     int probcut_beta = beta + 300;
-  if (!pvNode && !in_check && depth >= 5 && abs(beta) < mateScore  &&
+  if (!pvNode && !in_check && depth >= 7 && abs(beta) < mateScore  &&
       (!tt_hit || tt_depth + 3 < depth || tt_score >= probcut_beta)) {
     moves capture_promos[1];
     capture_promos->count = 0;
