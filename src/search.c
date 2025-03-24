ea59d5701a9eaa3fa802e756e411d170781a08a5
Eren Araz <lerenarazl@gmail.com>
2025-03-24 16:01:16 +0300

    bench: 5416396

--- src/search.c
+++ src/search.c
@@ -1076,14 +1076,14 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             }
 
             // Multicut
-            else if (score >= beta) {
+            else if (score >= singularBeta) {
                 // decrement ply
                 pos->ply--;
 
                 // decrement repetition index
                 pos->repetitionIndex--;
 
-                return beta;
+                return score;
             }
 
             // Negative Extensions
