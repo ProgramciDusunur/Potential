49a8f768d4c95144edeb5db7ead512e197018767
Eren Araz <lerenarazl@gmail.com>
2025-03-25 15:38:15 +0300

    bench: 6255753

--- src/search.c
+++ src/search.c
@@ -929,8 +929,8 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
     int legal_moves = 0;
 
     int probcut_beta = beta + 300;
-    if (!pvNode && !in_check && depth >= 7 && abs(beta) < mateScore  &&
-         (tt_depth + 3 < depth || tt_score >= probcut_beta)) {
+    if (!pvNode && !in_check && depth >= 8 && abs(beta) < mateScore  &&
+         (!tt_hit || tt_depth + 3 < depth || tt_score >= probcut_beta)) {
         moves capture_promos[1];
         capture_promos->count = 0;
         int score = -noEval;
@@ -992,11 +992,10 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
 
             if (score >= probcut_beta) {
                 writeHashEntry(score, 0, depth - 3, hashFlagAlpha, tt_pv, pos);
-
                 return score;
             }
         }
-         }
+    }
 
 
     // create move list instance
