d2b81fda96e0d911a099eeff56d1c5c55e0ed989
Eren Araz <lerenarazl@gmail.com>
2025-03-25 18:28:14 +0300

    try shallower late move reductions

--- src/search.c
+++ src/search.c
@@ -1143,6 +1143,8 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
 
             if (score > alpha && lmrReduction != 0) {
                 bool doDeeper = score > bestScore + DEEPER_LMR_MARGIN;
+                bool doShallower = score < bestScore + new_depth;
+                new_depth -= doShallower;
                 new_depth += doDeeper;
                 score = -negamax(-alpha - 1, -alpha, new_depth, pos, time, !cutNode);
             }
