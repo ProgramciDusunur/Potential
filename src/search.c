66a19d148b7d7998a5b897547b6eec1df6579dc8
Eren Araz <lerenarazl@gmail.com>
2025-03-26 16:34:42 +0300

    try 2 ply continuation history bench: 6317775

--- src/search.c
+++ src/search.c
@@ -167,6 +167,7 @@ int scoreMove(int move, board* position) {
 
         return quietHistory[position->side][getMoveSource(move)][getMoveTarget(move)] +
                 getContinuationHistoryScore(position, 1, move) +
+                    getContinuationHistoryScore(position, 2, move) +
                (position->ply == 0 * rootHistory[position->side][getMoveSource(move)][getMoveTarget(move)] * 4);
     }
     return 0;
