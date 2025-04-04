5ca46e8f2c381d45ca5c08fc988b8651c841c395
Eren Araz <lerenarazl@gmail.com>
2025-04-04 14:45:24 +0300

    start printing board again and try new solution bench: 7901111

--- src/uci.c
+++ src/uci.c
@@ -152,7 +152,7 @@ void parse_position(char *command, board* position) {
     }
 
     // print board
-    //pBoard(position);
+    pBoard(position);
 }
 
 
@@ -370,9 +370,9 @@ void read_input(time* time) {
     }
 }
 
-void communicate(time* time) {
+void communicate(time* time, board *pos) {
     // if time is up break here
-    if (time->timeset == 1 && getTimeMiliSecond() > time->hardLimit) {
+    if (time->timeset == 1 && getTimeMiliSecond() > time->hardLimit && pos->pvTable[0][0] != 0) {
         // tell engine to stop calculating
         time->stopped = 1;
     }
