996ec6cc783488ff233db4dfe6a2eedf59c61bc8
Eren Araz <lerenarazl@gmail.com>
2025-04-17 00:14:56 +0300

    bench: 5428224

--- src/search.c
+++ src/search.c
@@ -1236,7 +1236,6 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         lmrReduction /= 1024;
 
 
-
         int reduced_depth = myMAX(1, myMIN(new_depth - lmrReduction, new_depth));
 
         if(moves_searched >= LMR_FULL_DEPTH_MOVES &&
