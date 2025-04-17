7cfa75d76f7b2e4ff103a86cba011b9e8bfb694c
Eren Araz <lerenarazl@gmail.com>
2025-04-17 03:23:13 +0300

    if we are in a check then reduce less bench: 5183381

--- src/search.c
+++ src/search.c
@@ -49,6 +49,7 @@
   int QUIET_NON_PV_LMR_SCALER = 1024;
   int CUT_NODE_LMR_SCALER = 1024;
   int TT_PV_LMR_SCALER = 1024;
+  int IN_CHECK_LMR_SCALER = 1024;
   
   
   
@@ -1231,6 +1232,10 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         // Reduce Less
         if (tt_pv) {
             lmrReduction -= TT_PV_LMR_SCALER;
+        }
+        
+        if (in_check) {
+            lmrReduction -= IN_CHECK_LMR_SCALER;
         }
 
         lmrReduction /= 1024;
