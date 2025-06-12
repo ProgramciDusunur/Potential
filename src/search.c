bf99ee745e5a0d2d348987b23ad3ea2d4f49fe27
Eren Araz <lerenarazl@gmail.com>
2025-06-13 02:48:42 +0300

    bench: 7182638

--- src/search.c
+++ src/search.c
@@ -642,7 +642,7 @@ void scaleTime(time* time, uint8_t bestMoveStability, uint8_t evalStability, int
     double bestMoveScale[5] = {2.43, 1.35, 1.09, 0.88, 0.68};
     double evalScale[5] = {1.25, 1.15, 1.00, 0.94, 0.88};
     double not_bm_nodes_fraction = 
-       1 - (double)nodes_spent_table[move & 4095] / (double)searchNodes;
+       (double)nodes_spent_table[move & 4095] / (double)searchNodes;
     double node_scaling_factor = (1.5f - not_bm_nodes_fraction) * 1.35f;
     time->softLimit =
             myMIN(time->starttime + time->baseSoft * bestMoveScale[bestMoveStability] * 
