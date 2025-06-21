c915f29c293d61e8056ec252e89f3fff7698a26e
Eren Araz <lerenarazl@gmail.com>
2025-06-21 03:02:21 +0300

    try rfp failfirm return bernch: 9212532

--- src/search.c
+++ src/search.c
@@ -927,7 +927,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
     if (!pos->isSingularMove[pos->ply] && !tt_pv &&
         depth <= RFP_DEPTH && !pvNode && !in_check && (!tt_hit || ttAdjustedEval != static_eval) &&
         ttAdjustedEval - rfpMargin >= beta)
-        return ttAdjustedEval;
+        return (ttAdjustedEval * 3 + beta) / 4;
 
     // Null Move Pruning
     if (!pos->isSingularMove[pos->ply] && !pvNode &&
