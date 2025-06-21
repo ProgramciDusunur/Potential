4cb78c1984b44a5eb6af14b609682b24ec301877
Eren Araz <lerenarazl@gmail.com>
2025-06-22 02:45:08 +0300

    try probcut bench: 8092599

--- src/search.c
+++ src/search.c
@@ -1013,6 +1013,80 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         }
     }
 
+
+    // legal moves counter
+    int legal_moves = 0;
+
+    int probcut_beta = beta + 300;
+    if (!pvNode && !in_check && depth >= 5 && abs(beta) < mateScore  &&
+        (!tt_hit || tt_depth + 3 < depth || tt_score >= probcut_beta)) {
+            moves capture_promos[1];
+    capture_promos->count = 0;
+    int score = -noEval;
+
+    int16_t pc_see = probcut_beta - static_eval;
+    uint16_t pc_tt_move = SEE(pos, tt_move, pc_see) ? tt_move : 0;
+
+    moveGenerator(capture_promos, pos);
+
+      sort_moves(capture_promos, pc_tt_move, pos);
+
+    for (int count = 0; count < capture_promos->count; count++) {
+      int move = capture_promos->moves[count];
+      if (move == pos->isSingularMove[pos->ply]  ||
+          !(getMovePromoted(move) || getMoveCapture(move))) {
+        continue;
+      }
+
+        struct copyposition copyPosition;
+        // preserve board state
+        copyBoard(pos, &copyPosition);
+      // increment ply
+      pos->ply++;
+
+      // increment repetition index & store hash key
+      pos->repetitionIndex++;
+      pos->repetitionTable[pos->repetitionIndex] = pos->hashKey;
+
+      // make sure to make only legal moves
+        if (makeMove(capture_promos->moves[count], allMoves, pos) == 0) {
+        // decrement ply
+        pos->ply--;
+
+        // decrement repetition index
+        pos->repetitionIndex--;
+
+        // skip to next move
+        continue;
+      }
+
+        prefetch_hash_entry(pos->hashKey);
+        legal_moves++;
+
+
+        score = -quiescence(-probcut_beta, -probcut_beta + 1, pos, time);
+
+      if (score >= probcut_beta) {
+          negamax(-probcut_beta, -probcut_beta + 1, depth - 4, pos, time, !cutNode);
+      }
+
+        // decrement ply
+        pos->ply--;
+
+        // decrement repetition index
+        pos->repetitionIndex--;
+
+        // take move back
+        takeBack(pos, &copyPosition);
+
+      if (score >= probcut_beta) {
+          writeHashEntry(pos->hashKey, score, 0, depth - 3, hashFlagAlpha, tt_pv, pos);
+
+        return score;
+      }
+    }
+    }
+
     // create move list instance
     moves moveList[1], badQuiets[1];
     badQuiets->count = 0;
@@ -1032,11 +1106,6 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
     int moves_searched = 0;
 
     int bestScore = -infinity;
-
-    //bool skipQuiet = false;
-
-    // legal moves counter
-    int legal_moves = 0;
 
     // quiet move counter
     int quietMoves = 0;
