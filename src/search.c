dcf617c9022f78c1fd3a19fc7ff30eaeeb0f3d32
Eren Araz <lerenarazl@gmail.com>
2025-06-29 02:55:26 +0300

    bench: 8586549

--- src/search.c
+++ src/search.c
@@ -1175,10 +1175,10 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
                     depth += depth < 10;
                 }
 
-                int tripleMargin = TRIPLE_EXTENSION_MARGIN - 100 * pvNode;
+                int tripleMargin = TRIPLE_EXTENSION_MARGIN - 50 * pvNode;
 
                 // Triple Extension
-                if (!getMoveCapture(currentMove) && singularScore + tripleMargin < singularBeta) {
+                if (!getMoveCapture(currentMove) && singularScore + TRIPLE_EXTENSION_MARGIN < singularBeta) {
                     extensions++;
                 }
             // ╔═══════════════════════════╗
