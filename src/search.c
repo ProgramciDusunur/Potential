0bf785a57c1ae5b1c91ed8c961eaba6b55de4b1b
Eren Araz <lerenarazl@gmail.com>
2025-06-30 00:24:32 +0300

    tweak probcut and try again bench: 6961302

--- src/search.c
+++ src/search.c
@@ -1060,7 +1060,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
     int legal_moves = 0;
 
     int probcut_beta = beta + 300;
-    if (!pvNode && !in_check && depth >= 7 && abs(beta) < mateScore  &&
+    if (depth >= 7 && abs(beta) < mateScore  &&
         (!tt_hit || tt_depth + 3 < depth || tt_score >= probcut_beta)) {
             moves capture_promos[1];
     capture_promos->count = 0;
