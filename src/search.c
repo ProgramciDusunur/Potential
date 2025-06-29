881b5f8e9ed3c0a7a1eb0bf4264045b04b5f190f
Eren Araz <lerenarazl@gmail.com>
2025-06-29 23:40:38 +0300

    ----------------------------------------- Elo   | 0.85 +- 1.83 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=8MB LLR   | -2.96 (-2.94, 2.94) [0.00, 5.00] Games | N: 59116 W: 15496 L: 15352 D: 28268 Penta | [1379, 7105, 12423, 7295, 1356] https://chess.n9x.co/test/2091/ ----------------------------------------- new canditate
    bench: 8575035

--- src/search.c
+++ src/search.c
@@ -969,7 +969,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
     if (!pos->isSingularMove[pos->ply] && !tt_pv &&
         depth <= RFP_DEPTH && !pvNode && !in_check && (!tt_hit || ttAdjustedEval != static_eval) &&
         ttAdjustedEval - rfpMargin >= beta)
-        return (ttAdjustedEval * 3 + beta) / 4;
+        return ttAdjustedEval;
 
     // Null Move Pruning
     if (!pos->isSingularMove[pos->ply] && !pvNode &&
