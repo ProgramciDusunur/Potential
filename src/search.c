b19f5e53161e841884d1c22c22a51001dd20530b
Eren Araz <lerenarazl@gmail.com>
2025-07-02 13:51:06 +0300

    if tt hit and tt pv available, we are in a check then reduce less bench: 8931993

--- src/search.c
+++ src/search.c
@@ -50,6 +50,7 @@
   int CUT_NODE_LMR_SCALER = 2048;
   int TT_PV_LMR_SCALER = 1024;
   int TT_PV_FAIL_LOW_LMR_SCALER = 1024;
+  int TT_PV_IN_CHECK_LMR_SCALER = 1024;
   
   
   
@@ -1302,6 +1303,10 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         // Reduce Less
         if (tt_pv) {
             lmrReduction -= TT_PV_LMR_SCALER;
+        }
+
+        if (tt_pv && tt_hit && in_check) {
+            lmrReduction -= TT_PV_IN_CHECK_LMR_SCALER;
         }
 
         lmrReduction /= 1024;
