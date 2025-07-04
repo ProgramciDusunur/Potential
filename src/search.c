1c1279f8002f1492074f0e2fe9cab0716cd2a90d
Eren Araz <lerenarazl@gmail.com>
2025-07-04 05:03:13 +0300

    try depth 8 rfp bench: 8662801

--- src/search.c
+++ src/search.c
@@ -73,7 +73,7 @@
     ╚══════════════════════════════╝*/
   int RFP_MARGIN = 82;
   int RFP_IMPROVING_MARGIN = 65;
-  int RFP_DEPTH = 6;
+  int RFP_DEPTH = 8;
   
   
   /*╔══════════╗
@@ -1043,7 +1043,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             pos->nmpPly = 0;
 
             if (verificationScore >= beta) {
-                return score;
+                return (ttAdjustedEval * 3 + beta) / 4;
             }
         }
     }    
