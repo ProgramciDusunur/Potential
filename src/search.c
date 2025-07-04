5a32063c3b7ca5d5dfbd38a215d59936d26ed72f
Eren Araz <lerenarazl@gmail.com>
2025-07-04 22:39:15 +0300

    bench: 8339846

--- src/search.c
+++ src/search.c
@@ -1043,7 +1043,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             pos->nmpPly = 0;
 
             if (verificationScore >= beta) {
-                return (ttAdjustedEval * 3 + beta) / 4;
+                return (verificationScore * 3 + beta) / 4;
             }
         }
     }    
