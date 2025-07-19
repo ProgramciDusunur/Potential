a3570f00c2d11b07efaf825a981ecddb19eed0f9
Eren Araz <77514455+ProgramciDusunur@users.noreply.github.com>
2025-07-19 04:44:59 +0300

    Simplify double negext conditions (#350)
    * simplify double negext
    bench: 8861032
    
    * double negative extensions now nonfunctional :(((
    bench: 8861032

--- src/search.c
+++ src/search.c
@@ -1207,16 +1207,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
 
             // Negative Extensions
             else if (tt_score >= beta) {
-                extensions -= 2 + !pvNode;                
-                // Double Negative Extension
-                if (!pvNode && tt_score >= beta + DOUBLE_NEGATIVE_EXTENSION_MARGIN) {
-                    extensions -= 1;
-
-                    // High Depth Reduction
-                    depth -= depth > 12;
-                }
-               
-
+                extensions -= 2 + !pvNode;
             }
             
             // Cut Node Extension
