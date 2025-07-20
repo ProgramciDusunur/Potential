a4c4dea307f9c44f2d92cc7ddd68f2fab4e7fdfa
Eren Araz <lerenarazl@gmail.com>
2025-07-20 17:00:47 +0300

    tweak and try again bench: 8445309

--- src/history.h
+++ src/history.h
@@ -41,7 +41,6 @@ void updateCaptureHistoryMalus(board *position, int depth, moves *noisyMoves, in
 void updateSingleCHScore(board *pos, int move, const int offSet, const int bonus);
 void updateAllCH(board *pos, int move, int bonus);
 int getHistoryBonus(int depth);
-int getCaptureHistoryBonus(int depth);
 void updateContinuationHistory(board *pos, int bestMove, int depth, moves *badQuiets);
 int getContinuationHistoryScore(board *pos, int offSet, int move);
 void clearQuietHistory(void);
