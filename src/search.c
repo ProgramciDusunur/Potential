9fb804ce45c7772271769203209e69c5ffcaf0da
Eren Araz <lerenarazl@gmail.com>
2025-07-23 20:55:28 +0300

    try tt score fail low Singular Extensions bench: 9220309

--- src/search.c
+++ src/search.c
@@ -1210,6 +1210,10 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
             else if (tt_score >= beta) {
                 extensions -= 2 + !pvNode;
             }
+
+            else if (tt_score <= alpha) {
+                extensions -= 1;
+            }
             
             // Cut Node Extension
             else if (cutNode) {
