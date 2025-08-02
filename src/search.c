dab5be5b7a172aca9b9479ca26d63079bc38c626
Eren Araz <lerenarazl@gmail.com>
2025-08-02 18:10:39 +0300

    ------------------------------------------------------------------------ Elo   | 0.32 +- 2.84 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=32MB LLR   | -2.09 (-2.94, 2.94) [0.00, 5.00] Games | N: 24986 W: 6641 L: 6618 D: 11727 Penta | [599, 3043, 5173, 3092, 586] https://rektdie.pythonanywhere.com/test/579/ ------------------------------------------------------------------------ The test looks like fail yellow, so we can try more aggressive and safe cut node lmr bench: 8699243

--- src/search.c
+++ src/search.c
@@ -47,7 +47,7 @@
   int QUIET_HISTORY_LMR_MINIMUM_SCALER = 3072;
   int QUIET_HISTORY_LMR_MAXIMUM_SCALER = 3072;
   int QUIET_NON_PV_LMR_SCALER = 1024;
-  int CUT_NODE_LMR_SCALER = 2048;
+  int CUT_NODE_LMR_SCALER = 3072;
   int TT_PV_LMR_SCALER = 1024;
   int TT_PV_FAIL_LOW_LMR_SCALER = 1024;
   
@@ -1271,7 +1271,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         /* All Moves */
 
         // Reduce More
-        if (cutNode) {
+        if (!tt_pv && cutNode) {
             lmrReduction += CUT_NODE_LMR_SCALER;
         }
 
