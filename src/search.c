c16757d74d9478fbe780ef749d190c2c2530e57c
Eren Araz <lerenarazl@gmail.com>
2025-08-05 19:19:37 +0300

    make the singularity margin larger for captures, requiring them to be more clearly better than alternatives to get an extension bench: 9134727

--- src/search.c
+++ src/search.c
@@ -1149,7 +1149,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         if (pos->ply < depth * 2 && !rootNode && depth >= SE_DEPTH + tt_pv && currentMove == tt_move && !pos->isSingularMove[pos->ply] &&
             tt_depth >= depth - SE_TT_DEPTH_SUBTRACTOR && tt_flag != hashFlagBeta &&
             abs(tt_score) < mateScore) {
-            const int singularBeta = tt_score - depth * 5 / 8;
+            const int singularBeta = tt_score - (depth * 5 + 12 * !notTactical) / 8;
             const int singularDepth = (depth - 1) / 2;
 
 
