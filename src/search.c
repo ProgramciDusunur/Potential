ea9208ff2853b2c38e31320d3608798869dc6f9c
Eren Araz <lerenarazl@gmail.com>
2025-08-08 17:34:27 +0300

    if our tt move is capture then reduce less bench: 10293013

--- src/search.c
+++ src/search.c
@@ -50,6 +50,7 @@
   int CUT_NODE_LMR_SCALER = 2048;
   int TT_PV_LMR_SCALER = 1024;
   int TT_PV_FAIL_LOW_LMR_SCALER = 1024;
+  int TT_CAPTURE_LMR_SCALER = 1024;
   
   
   /*╔═══════════════════════╗
@@ -1293,6 +1294,11 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         // Reduce Less
         if (tt_pv) {
             lmrReduction -= TT_PV_LMR_SCALER + (512 * pvNode);
+        }
+
+        if (tt_hit && getMoveCapture(tt_move)) {
+            lmrReduction -= TT_CAPTURE_LMR_SCALER;
+
         }
 
         lmrReduction /= 1024;
