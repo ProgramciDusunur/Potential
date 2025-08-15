da580d028639acd64cd3f0d8ea701b0d9c099c2a
Eren Araz <lerenarazl@gmail.com>
2025-08-15 21:27:29 +0300

    just reduce when tt hit is available bench: 9107556

--- src/search.c
+++ src/search.c
@@ -1115,7 +1115,7 @@ int negamax(int alpha, int beta, int depth, board* pos, time* time, bool cutNode
         int lmrDepth = myMAX(0, depth - getLmrReduction(depth, legal_moves, notTactical) + moveHistory / 8192);
 
         // Prune More
-        if (tt_pv) {
+        if (tt_hit && tt_pv) {
             lmrDepth -= 1;
         }
 
