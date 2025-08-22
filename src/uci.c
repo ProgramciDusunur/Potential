e310bdc0195ced24ba626c03b68ecf8752d1fbf2
Eren Araz <lerenarazl@gmail.com>
2025-08-23 01:06:50 +0300

    bench: 15791423

--- src/uci.c
+++ src/uci.c
@@ -411,7 +411,7 @@ void uciProtocol(int argc, char *argv[], board *position, my_time *time_ctrl) {
 
     if (argc >= 2 && strncmp(argv[1], "bench", 5) == 0) {
         printf("bench running..\n");
-        benchmark(15, position, time_ctrl);
+        benchmark(14, position, time_ctrl);
         printf("\n");
         fflush(NULL);
         return;
