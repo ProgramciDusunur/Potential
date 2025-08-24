ae1b12fa64a4c4a711447731bfb90f838ca11d0e
Eren Araz <lerenarazl@gmail.com>
2025-08-25 00:23:05 +0300

    just tweak bench: 20274200

--- src/search.c
+++ src/search.c
@@ -978,8 +978,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
     if (ttAdjustedEval != static_eval && (tt_move && tt_hit)) {
         if (corrplexity) {
             depth++;
-        }
-        if (correctionValue <= -100 && depth >= 3) {            
+        } else if (correctionValue <= -41 && depth >= 7) {
             // Negative Corrplexity Extension            
             depth--;
         }
