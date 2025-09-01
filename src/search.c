936d5741efea9604115427cb6aa07a38c7feeff4
Eren Araz <lerenarazl@gmail.com>
2025-09-01 16:25:05 +0300

    try reverse approach bench: 10905423

--- src/search.c
+++ src/search.c
@@ -1303,7 +1303,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
             int ttMoveTrust = tt_move ? 64 + depth * 16 : 0;
 
             // if the move have good history decrease reduction other hand the move have bad history then reduce more
-            int moveHistoryReduction = moveHistory / (QUIET_HISTORY_LMR_DIVISOR - ttMoveTrust);
+            int moveHistoryReduction = moveHistory / (QUIET_HISTORY_LMR_DIVISOR + ttMoveTrust);
             lmrReduction -= clamp(moveHistoryReduction * 1024, -QUIET_HISTORY_LMR_MINIMUM_SCALER, QUIET_HISTORY_LMR_MINIMUM_SCALER);
         }
 
