7d21a17dcc5dbb19e00e590ad05a5c0f2dd9f957
Eren Araz <lerenarazl@gmail.com>
2025-09-01 17:58:12 +0300

    bench: 5257755

--- src/search.c
+++ src/search.c
@@ -975,7 +975,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
 
     // ~~~~ Corrplexity Extension ~~~~ //
     if (corrplexity && ttAdjustedEval != static_eval && (tt_move && tt_hit)) {                
-        depth += 1 + (abs(raw_eval - static_eval) > 200);
+        depth += 1 + (abs(raw_eval - static_eval) > 246 && !in_check && pvNode);
         
     }
 
