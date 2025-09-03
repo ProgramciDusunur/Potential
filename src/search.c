3a06dbb8af724bdf551be156578d570e0e2426ff
Eren Araz <lerenarazl@gmail.com>
2025-09-03 06:31:33 +0300

    experiment experiment experiment.. bench: 5411389

--- src/search.c
+++ src/search.c
@@ -936,6 +936,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
     bool improving = false;
 
     bool corrplexity = abs(raw_eval - static_eval) > 82;
+    int correctionValue = raw_eval - static_eval;
 
     int pastStack = -1;
 
@@ -973,8 +974,13 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
     // ╚═══════════════════════════╝
 
     // ~~~~ Corrplexity Extension ~~~~ //
-    if (corrplexity && ttAdjustedEval != static_eval && (tt_move && tt_hit)) {
-        depth++;
+    if (ttAdjustedEval != static_eval && tt_hit) {
+        if (corrplexity) {
+            depth++;
+        } else if (correctionValue <= -82 && depth >= 3 && tt_score >= score) {
+            // Negative Corrplexity Extension            
+            depth--;
+        }        
     }
 
     improving |= pos->staticEval[pos->ply] >= beta + 100;
