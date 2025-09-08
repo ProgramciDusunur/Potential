7e914a4b522077e06a63674c45dc612221813809
Eren Araz <lerenarazl@gmail.com>
2025-09-08 04:01:26 +0300

    simplify more negext in non pv nodes bench: 16431998

--- src/search.c
+++ src/search.c
@@ -1226,7 +1226,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
 
             // Negative Extensions
             else if (tt_score >= beta) {
-                extensions -= 2 + !pvNode;
+                extensions -= 2;
             }
             
             // Cut Node Extension
