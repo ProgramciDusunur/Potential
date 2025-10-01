c3ff1e97c0fe93fc319f59455dff15147e0a8534
Eren Araz <lerenarazl@gmail.com>
2025-10-01 18:11:00 +0300

    try more safe depth bench: 3583552

--- src/search.c
+++ src/search.c
@@ -1137,7 +1137,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
     int legal_moves = 0;
 
     int probcut_beta = beta + 300;
-    if (!pvNode && !in_check && depth >= 5 && abs(beta) < mateScore  && !pos->isSingularMove[pos->ply] &&
+    if (!pvNode && !in_check && depth >= 7 && abs(beta) < mateScore  && !pos->isSingularMove[pos->ply] &&
         (!tt_hit || tt_depth + 3 < depth || tt_score >= probcut_beta)) {
             moves capture_promos[1];
     capture_promos->count = 0;    
