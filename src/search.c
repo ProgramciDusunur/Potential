3d5cded95f5e304f97743601d1d5ea11506cf2ad
Eren Araz <lerenarazl@gmail.com>
2025-10-02 16:48:52 +0300

    bench: 41223285

--- src/search.c
+++ src/search.c
@@ -1136,7 +1136,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
     // legal moves counter
     int legal_moves = 0;
 
-    int probcut_beta = beta + 400;
+    int probcut_beta = beta + 250;
     if (!pvNode && !in_check && depth >= 5 && abs(beta) < mateScore  && !pos->isSingularMove[pos->ply] &&
         (!tt_hit || tt_depth + 3 < depth || tt_score >= probcut_beta)) {
             moves capture_promos[1];
@@ -1183,9 +1183,9 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
 
         int probcut_value = -quiescence(-probcut_beta, -probcut_beta + 1, pos, time);
 
-      if (probcut_value >= probcut_beta && probcut_depth > 0) {
-          probcut_value = -negamax(-probcut_beta, -probcut_beta + 1, probcut_depth, pos, time, !cutNode);
-      }
+        if (probcut_value >= probcut_beta) {
+            probcut_value = -negamax(-probcut_beta, -probcut_beta + 1, probcut_depth, pos, time, !cutNode);
+        }
 
         // decrement ply
         pos->ply--;
