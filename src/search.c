c408bc2755973caeccb124d45b22a40d2e6df8d6
Eren Araz <lerenarazl@gmail.com>
2025-10-03 06:25:35 +0300

    try more soft formula and margin bench: 6319507

--- src/search.c
+++ src/search.c
@@ -1047,7 +1047,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
     // Null Move Pruning
     if (!pos->isSingularMove[pos->ply] && !pvNode &&
         depth >= NMP_DEPTH && !in_check && !rootNode &&
-            ttAdjustedEval >= beta + 246 - 20 * depth &&
+            ttAdjustedEval >= beta + 60 - 6 * depth &&
             pos->ply >= pos->nmpPly &&
             !justPawns(pos)) {
         struct copyposition copyPosition;
