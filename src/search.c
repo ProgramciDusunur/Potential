330b4bc5060a5daa509df1005b51a84906052c3d
Eren Araz <lerenarazl@gmail.com>
2025-10-06 02:27:55 +0300

    tweak futility lmr with move history bench: 8851814

--- src/search.c
+++ src/search.c
@@ -1437,7 +1437,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
             }
 
             // Futility LMR
-            lmrReduction += (static_eval + 164 + 82 * depth <= alpha && !in_check) * 1024;
+            lmrReduction += (static_eval + 164 + 82 * depth + moveHistory / 32 <= alpha && !in_check) * 1024;
 
 
             // if the move have good history decrease reduction other hand the move have bad history then reduce more
