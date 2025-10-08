bf074b956d4bc54d9b88e5376d85746d55bafcba
Eren Araz <lerenarazl@gmail.com>
2025-10-08 03:24:16 +0300

    do probcut in pv nodes bench: 5853731

--- src/search.c
+++ src/search.c
@@ -1136,8 +1136,8 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
     // legal moves counter
     int legal_moves = 0;
 
-    int probcut_beta = beta + 150 - 30 * improving;
-    if (!pvNode && !in_check && depth >= 5 && abs(beta) < mateScore  && !pos->isSingularMove[pos->ply] &&
+    int probcut_beta = beta + 150 + 50 * pvNode - 30 * improving;
+    if (!in_check && depth >= 5 && abs(beta) < mateScore  && !pos->isSingularMove[pos->ply] &&
         (!tt_hit || tt_depth + 3 < depth || tt_score >= probcut_beta)) {
             moves capture_promos[1];
     capture_promos->count = 0;
