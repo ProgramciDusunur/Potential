dcd72e04d6804dcd3ed82e4f02eb52881ded851f
Eren Araz <lerenarazl@gmail.com>
2025-10-09 03:33:08 +0300

    YELLOW ARMAGEDDON TIME! bench: 8286083

--- src/search.c
+++ src/search.c
@@ -1198,6 +1198,8 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
         takeBack(pos, &copyPosition);
 
       if (probcut_value >= probcut_beta) {
+          // Update capture history according to probcut result
+          updateCaptureHistory(pos, move, probcut_depth);
           writeHashEntry(pos->hashKey, probcut_value, move, probcut_depth, hashFlagAlpha, tt_pv, pos);
 
         return probcut_value;
