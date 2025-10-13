619a14a837de8d582f4bfa03eadab6c39ae9ca6f
Eren Araz <lerenarazl@gmail.com>
2025-10-13 12:14:11 +0300

    Elo   | -0.34 +- 2.21 (95%) SPRT  | 40.0+0.40s Threads=1 Hash=64MB LLR   | -2.95 (-2.94, 2.94) [0.00, 4.00] Games | N: 34648 W: 8484 L: 8518 D: 17646 Penta | [512, 4263, 7818, 4209, 522] https://rektdie.pythonanywhere.com/test/1485/
    Elo   | 0.66 +- 1.46 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | -2.29 (-2.94, 2.94) [0.00, 4.00]
    Games | N: 77290 W: 18847 L: 18700 D: 39743
    Penta | [1033, 9515, 17497, 9472, 1128]
    https://rektdie.pythonanywhere.com/test/1386/
    
    bench: 45950142

--- src/search.c
+++ src/search.c
@@ -228,7 +228,8 @@ int scoreMove(int move, board* position) {
         // score move by MVV LVA lookup [source piece][target piece]
         captureScore += mvvLva[getMovePiece(move)][target_piece];
 
-        captureScore += captureHistory[getMovePiece(move)][getMoveTarget(move)][position->mailbox[getMoveTarget(move)]];
+        captureScore += captureHistory[getMovePiece(move)][getMoveTarget(move)][position->mailbox[getMoveTarget(move)]]
+        [is_square_threatened(position, getMoveSource(move))][is_square_threatened(position, getMoveTarget(move))];
 
         captureScore += SEE(position, move, SEE_MOVE_ORDERING_THRESHOLD) ? 1000000000 : -1000000;
 
@@ -1197,7 +1198,9 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
         takeBack(pos, &copyPosition);
 
       if (probcut_value >= probcut_beta) {
-          writeHashEntry(pos->hashKey, probcut_value, move, probcut_depth, hashFlagAlpha, tt_pv, pos);
+        // Update capture history according to probcut result
+        updateCaptureHistory(pos, move, probcut_depth);
+        writeHashEntry(pos->hashKey, probcut_value, move, probcut_depth, hashFlagAlpha, tt_pv, pos);
 
         return probcut_value;
       }
