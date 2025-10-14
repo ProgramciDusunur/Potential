5b3d3e1e01adc25534b11e01e899ff6072317113
Eren Araz <lerenarazl@gmail.com>
2025-10-14 15:25:48 +0300

    since the gain we get from double extension creates a lot of saturation, we try a combination of triple and quadruple for a meaningful gain bench: 6794850

--- src/search.c
+++ src/search.c
@@ -1335,7 +1335,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
                 }
 
                 // Triple Extension
-                int tripleMargin = TRIPLE_EXTENSION_MARGIN + 80 * !notTactical;
+                int tripleMargin = TRIPLE_EXTENSION_MARGIN + 80 * !notTactical - (moveHistory / 1024 * notTactical);
 
                 if (singularScore <= singularBeta - tripleMargin) {
                     extensions++;
@@ -1354,7 +1354,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
                 // ╚═══════════════════════════╝
 
                 // ~~~~ Quadruple Extension ~~~~ //
-                int quadrupleMargin = QUADRUPLE_EXTENSION_MARGIN + 170 * !notTactical;
+                int quadrupleMargin = QUADRUPLE_EXTENSION_MARGIN + 170 * !notTactical - (moveHistory / 2048 * notTactical);
 
                 if (singularScore <= singularBeta - quadrupleMargin) {
                     extensions++;
