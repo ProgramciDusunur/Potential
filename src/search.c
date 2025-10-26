e5d98665796eea2d8cb64090fcd88afcb96173c1
Eren Araz <lerenarazl@gmail.com>
2025-10-26 21:18:54 +0300

    in check lmr attempt 12341531241312 (!) please pass bench: 4967905

--- src/search.c
+++ src/search.c
@@ -56,6 +56,7 @@
   int TT_PV_FAIL_LOW_LMR_SCALER = 1024;
   int TT_CAPTURE_LMR_SCALER = 1024;
   int GOOD_EVAL_LMR_SCALER = 1024;
+  int IN_CHECK_LMR_SCALER = 1024;
   int LMR_FUTILITY_OFFSET[] = {0, 164, 82, 41, 20, 10};
   
   
@@ -1494,8 +1495,12 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
         if (tt_pv) {
             lmrReduction -= TT_PV_LMR_SCALER + (512 * pvNode) + (256 * improving);
         }
-        
 
+        if (in_check) {
+            lmrReduction -= IN_CHECK_LMR_SCALER;
+        }
+        
+        
         lmrReduction /= 1024;
 
         int reduced_depth = myMAX(1, myMIN(new_depth - lmrReduction, new_depth));
