40649612d8ea1f44f554692e59c403e78861b4cd
Eren Araz <lerenarazl@gmail.com>
2025-10-29 04:55:38 +0300

    tweak good eval lmr bench: 5150115

--- src/search.c
+++ src/search.c
@@ -1462,7 +1462,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
             lmrReduction += TT_CAPTURE_LMR_SCALER;
         }
 
-        if (enemy_has_no_threats && !in_check && static_eval - 365 > beta) {
+        if (enemy_has_no_threats && !in_check && ttAdjustedEval - 365 >= beta + 120 - 25 * depth) {
             lmrReduction += GOOD_EVAL_LMR_SCALER;
         }
 
