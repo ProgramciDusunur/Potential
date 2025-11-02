389250829e404e2bac626cc59352d21b6bc4696e
Eren Araz <lerenarazl@gmail.com>
2025-11-03 02:07:36 +0300

    ``` Elo   | 1.11 +- 1.30 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=32MB LLR   | -1.56 (-2.94, 2.94) [0.00, 4.00] Games | N: 127028 W: 36233 L: 35827 D: 54968 Penta | [3554, 14995, 26076, 15269, 3620] ``` https://rektdie.pythonanywhere.com/test/1729/
    need more effective margin
    
    bench: 5481837

--- src/search.c
+++ src/search.c
@@ -1169,14 +1169,15 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
             sort_moves(capture_promos, tt_move, pos);
 
             for (int count = 0; count < capture_promos->count; count++) {
-                int move = capture_promos->moves[count];  
+                int move = capture_promos->moves[count];
+                int moveHistory = captureHistory[getMovePiece(move)][getMoveTarget(move)][pos->mailbox[getMoveTarget(move)]];
         
                 if (!SEE(pos, move, 100)) {
                     continue;
                 }
 
                 // Noisy Futility Pruning
-                int noisyFPMargin = static_eval + 200 + 200 * depth;
+                int noisyFPMargin = static_eval + 200 + 200 * depth + moveHistory / 32;
                 if (!pvNode && !in_check && noisyFPMargin <= alpha) {
                     continue;
                 }
