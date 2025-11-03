0de1d0f75bb38586b13c3c8599fd198092631712
Eren Araz <lerenarazl@gmail.com>
2025-11-03 23:54:16 +0300

    add Noisy Futility Pruning bench: 5182865

--- src/search.c
+++ src/search.c
@@ -1290,23 +1290,33 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
 
         bool isNotMated = bestScore > -mateScore;
 
-        if (!rootNode && notTactical && isNotMated) {
+        if (!rootNode && isNotMated) {
 
-            int lmpThreshold = (LMP_BASE + LMP_MULTIPLIER * lmrDepth * lmrDepth) / (2 - improving);
+            if (notTactical) {
+                int lmpThreshold = (LMP_BASE + LMP_MULTIPLIER * lmrDepth * lmrDepth) / (2 - improving);
 
-            // Late Move Pruning
-            if (legal_moves>= lmpThreshold) {
-                continue;
+                // Late Move Pruning
+                if (legal_moves>= lmpThreshold) {
+                    continue;
+                }
+
+                // Futility Pruning
+                if (lmrDepth <= FP_DEPTH && !pvNode && !in_check && (static_eval + FUTILITY_PRUNING_OFFSET[clamp(lmrDepth, 1, 5)]) + FP_MARGIN * lmrDepth + moveHistory / 32 <= alpha) {
+                    continue;
+                }
+                // Quiet History Pruning
+                if (lmrDepth <= 4 && !in_check && moveHistory < lmrDepth * lmrDepth * -2048) {
+                    break;
+                }
+            } else {
+                // Noisy Futility Pruning
+                int noisyFPMargin = static_eval + 200 + 200 * depth;
+                if (depth <= 5 && !pvNode && !in_check && noisyFPMargin <= alpha) {                   
+                    continue;
+                }
             }
 
-            // Futility Pruning
-            if (lmrDepth <= FP_DEPTH && !pvNode && !in_check && (static_eval + FUTILITY_PRUNING_OFFSET[clamp(lmrDepth, 1, 5)]) + FP_MARGIN * lmrDepth + moveHistory / 32 <= alpha) {
-                continue;
-            }
-            // Quiet History Pruning
-            if (lmrDepth <= 4 && !in_check && moveHistory < lmrDepth * lmrDepth * -2048) {
-                break;
-            }
+            
 
         }
 
