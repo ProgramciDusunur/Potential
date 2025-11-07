64774555a0818ce9530256c88628d1c0a9204922
Eren Araz <lerenarazl@gmail.com>
2025-11-07 16:11:06 +0300

    do more cut node lmr if tt move is not available bench: 5019598

--- src/search.c
+++ src/search.c
@@ -1468,7 +1468,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
 
         // Reduce More
         if (cutNode) {
-            lmrReduction += CUT_NODE_LMR_SCALER;
+            lmrReduction += CUT_NODE_LMR_SCALER + !tt_move * 1024;
         }
 
         if (tt_pv && tt_hit && tt_score <= alpha) {
