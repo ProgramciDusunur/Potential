ee0273edc87dba5d286682e1bfb583fb2f634835
Eren Araz <77514455+ProgramciDusunur@users.noreply.github.com>
2025-11-07 20:25:56 +0300

    Reset king rook pawn correction history array after the ucinewgame command (#398)
    bench: 5019598

--- src/uci.c
+++ src/uci.c
@@ -471,6 +471,7 @@ void uciProtocol(int argc, char *argv[], board *position, my_time *time_ctrl) {
             memset(MAJOR_CORRECTION_HISTORY, 0, sizeof(MAJOR_CORRECTION_HISTORY));
             memset(NON_PAWN_CORRECTION_HISTORY, 0, sizeof(NON_PAWN_CORRECTION_HISTORY));
             memset(contCorrhist, 0, sizeof(contCorrhist));
+            memset(krpCorrhist, 0, sizeof(krpCorrhist));
 
             // call parse position function
             parse_position("position startpos", position);
