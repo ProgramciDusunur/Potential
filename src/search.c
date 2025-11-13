cf3683a769647716717b798085b9577e47c94351
Eren Araz <lerenarazl@gmail.com>
2025-11-13 20:02:32 +0300

    try slightly faster capture ordering in QS bench: 5168953

--- src/search.c
+++ src/search.c
@@ -313,29 +313,14 @@ int quiescenceScoreMove(int move, board* position) {
     if (getMoveCapture(move)) {
         // init target piece
 
+        // init target piece
         int target_piece = P;
 
-        // pick up bitboard piece index ranges depending on side
-        int start_piece, end_piece;
-
-        // pick up side to move
-        if (position->side == white) {
-            start_piece = p;
-            end_piece = k;
-        }
-        else {
-            start_piece = P;
-            end_piece = K;
-        }
-
-        // loop over bitboards opposite to the current side to move
-        for (int bb_piece = start_piece; bb_piece <= end_piece; bb_piece++) {
-            // if there's a piece on the target square
-            if (getBit(position->bitboards[bb_piece], getMoveTarget(move))) {
-                // remove it from corresponding bitboard
-                target_piece = bb_piece;
-                break;
-            }
+        uint8_t bb_piece = position->mailbox[getMoveTarget(move)];
+        // if there's a piece on the target square
+        if (bb_piece != NO_PIECE &&
+            getBit(position->bitboards[bb_piece], getMoveTarget(move))) {
+            target_piece = bb_piece;
         }
 
         // score move by MVV LVA lookup [source piece][target piece]
