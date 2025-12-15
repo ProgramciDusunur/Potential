2cf3c1980823edb27ff362c7d488140ee4976412
Eren Araz <lerenarazl@gmail.com>
2025-12-15 03:33:44 +0300

    do refutation move ordering in captures bench: 6059745

--- src/search.c
+++ src/search.c
@@ -286,7 +286,7 @@ int scoreMove(uint16_t move, board* position) {
         // pawn history
         quiet_score += pawnHistory[position->pawnKey % 2048][position->mailbox[getMoveSource(move)]][getMoveTarget(move)];
         // NMP refutation move
-        //quiet_score += getMoveSource(move) == getMoveTarget(position->nmp_refutation_move[position->ply]) ? 500000 : 0;
+        quiet_score += getMoveSource(move) == getMoveTarget(position->nmp_refutation_move[position->ply]) ? 4096 : 0;
 
         return quiet_score;        
     }
