9d6f22d1d185dd77d91d4a89acebe7a0e9bcee95
Eren Araz <lerenarazl@gmail.com>
2025-12-17 17:19:33 +0300

    do less reduction in pv nodes bench: 5754001

--- src/search.c
+++ src/search.c
@@ -1430,7 +1430,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
 
         // Reduce Less
         if (tt_pv) {
-            lmrReduction -= TT_PV_LMR_SCALER + (512 * pvNode) + (256 * improving);
+            lmrReduction -= TT_PV_LMR_SCALER + (1024 * pvNode) + (256 * improving);
         }
         
 
