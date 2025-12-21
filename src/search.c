3f5c72911c942585aa0eecb7a07715c563c4db28
Eren Araz <lerenarazl@gmail.com>
2025-12-21 04:53:29 +0300

    take 3 bench: 5297379

--- src/search.c
+++ src/search.c
@@ -1223,7 +1223,10 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
 
         int lmrDepth = myMAX(0, depth - getLmrReduction(depth, legal_moves, notTactical) + (moveHistory / 8192 * notTactical));
 
-
+        // Prune more on tt pv nodes (*scaler)
+        if (tt_pv) {
+            lmrDepth -= 2;
+        }
 
         bool isNotMated = bestScore > -mateFound;
 
