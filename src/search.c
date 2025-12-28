41e88f129213ca8c2c1010cc4d4827d05bd1aa29
Eren Araz <lerenarazl@gmail.com>
2025-12-29 02:57:17 +0300

    tweak Late Move Pruning with corrplexity bench: 4287845

--- src/search.c
+++ src/search.c
@@ -1191,7 +1191,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
 
         if (!rootNode && notTactical && isNotMated) {
 
-            int lmpThreshold = (LMP_BASE + LMP_MULTIPLIER * lmrDepth * lmrDepth) / (2 - improving);
+            int lmpThreshold = (LMP_BASE + LMP_MULTIPLIER * lmrDepth * lmrDepth) / (2 - (improving || corrplexity));
 
             // Late Move Pruning
             if (legal_moves>= lmpThreshold) {
