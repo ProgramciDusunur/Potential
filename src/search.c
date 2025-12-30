472fbb012fc266303018325ac9ab54a4390ec029
Eren Araz <lerenarazl@gmail.com>
2025-12-30 17:23:26 +0300

    add recapture extensin in SE bench: 6041375

--- src/search.c
+++ src/search.c
@@ -1214,6 +1214,8 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
         if (lmrDepth <= SEE_DEPTH && legal_moves > 0 && !SEE(pos, currentMove, seeThreshold))
             continue;
 
+        int previous_move_target_square = getMoveTarget(pos->move[myMAX(0, pos->ply - 1)]);    
+
         int extensions = 0;
 
         // Singular Extensions
@@ -1288,6 +1290,11 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
             // Negative Extensions
             else if (tt_score >= beta) {
                 extensions -= 2 + !pvNode;
+            }
+
+            // Recapture Extension
+            else if (pvNode && !notTactical && getMoveTarget(tt_move) == previous_move_target_square) {
+                extensions += 1;
             }
             
             // Cut Node Extension
