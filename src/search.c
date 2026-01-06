714bec23a1ee47bcede152c758e1690f0e26caab
Eren Araz <lerenarazl@gmail.com>
2026-01-07 02:58:52 +0300

    do more aggressive Late Move Pruning bench: 5809559

--- src/search.c
+++ src/search.c
@@ -64,8 +64,8 @@
   /*╔═══════════════════════╗
     ║ Late Move Pruning     ║
     ╚═══════════════════════╝*/
-  int LMP_BASE = 4;
-  int LMP_MULTIPLIER = 3;
+  int LMP_BASE = 3;
+  int LMP_MULTIPLIER = 2;
 
 /*╔═════════════╗
   ║   Probcut   ║
