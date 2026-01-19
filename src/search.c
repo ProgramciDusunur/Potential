5fb14e015cda007810c9e60b9305f74ab40be1ac
Eren Araz <lerenarazl@gmail.com>
2026-01-19 16:45:00 +0300

    give larger values bench: 5684739

--- src/search.c
+++ src/search.c
@@ -1541,7 +1541,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
             return get_draw_score(pos);
     } else if (bestMove) {
         if (!pvNode) {
-            ttMoveHistory[pos->side] += bestMove == tt_move ? 400 : -350;
+            ttMoveHistory[pos->side] += bestMove == tt_move ? 600 : -450;
         }
         
     }
