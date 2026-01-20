2ce162c63add48fcd06cce072061d7cbd9d00537
Eren Araz <lerenarazl@gmail.com>
2026-01-20 05:40:46 +0300

    an idea from Stockfish engine bench: 6014987

--- src/search.c
+++ src/search.c
@@ -1273,7 +1273,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
 
             // Negative Extensions
             else if (tt_score >= beta) {
-                extensions -= 2 + !pvNode;
+                extensions -= (cutNode ? 3 : 2) + !pvNode;
             }
 
             // ╔══════════════════════════════╗
