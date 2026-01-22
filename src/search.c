07fde585357b05a7c0035b19c84d2e15447a8bef
Eren Araz <lerenarazl@gmail.com>
2026-01-22 15:51:05 +0300

    an idea from Reckless engine bench: 4855893

--- src/search.c
+++ src/search.c
@@ -852,6 +852,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
 
     bool corrplexity = abs(raw_eval - static_eval) > 82;
     int corrplexity_value = abs(raw_eval - static_eval);
+    int correction_value = get_correction_value(pos);
 
     int pastStack = -1;
 
@@ -1442,6 +1443,10 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
             if (currentMove == tt_move && pos->rootDepth > 8 && tt_depth > 1) {
                 new_depth = myMAX(new_depth, 1);
             }
+
+            if (!tt_move && cutNode) {
+                new_depth -= 1;
+            }
             
             score = -negamax(-alpha - 1, -alpha, new_depth, pos, time, !cutNode);
         }
@@ -1548,6 +1553,10 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
             hashFlag = hashFlagBeta;
         }
 
+        if (!in_check && !pos->isSingularMove[pos->ply]) {
+            static_eval += get_correction_value(pos) - correction_value;
+        }
+        
         if (!in_check && (bestMove == 0 || !getMoveCapture(bestMove)) &&
             !(hashFlag == hashFlagAlpha && bestScore <= static_eval) &&
             !(hashFlag == hashFlagBeta && bestScore >= static_eval)) {
