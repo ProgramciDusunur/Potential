2bef352dde5a33a8787c8a07bb4632c29cd61c25
Eren Araz <lerenarazl@gmail.com>
2026-01-27 10:26:53 +0300

    let's be more cautious and aggressive bench: 6142177

--- src/search.c
+++ src/search.c
@@ -1358,7 +1358,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, bool cutN
             if (singularScore < singularBeta) {
                 extensions++;
 
-                int correction_adj = abs(correction_value) / 2000;                
+                int correction_adj = abs(correction_value) / 2750;                
 
                 // Double Extension                
                 int doubleMargin = DOUBLE_EXTENSION_MARGIN + 40 * !notTactical - (moveHistory / 512) - (pawnHistoryValue / 384) - (corrplexity_value / 16);
