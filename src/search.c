c6dd7f38829ec5e2adeec40dd69e52d3756e4c41
Eren Araz <lerenarazl@gmail.com>
2026-02-16 02:32:59 +0300

    tweak RFP return value bench: 12015289

--- src/search.c
+++ src/search.c
@@ -1024,9 +1024,9 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, SearchSta
 
     // Reverse Futility Pruning
     if (!ss->singular_move && rfp_tt_pv_decision &&
-        depth <= RFP_DEPTH && !pvNode && !in_check && (!tt_hit || ttAdjustedEval != static_eval) &&
+        depth <= RFP_DEPTH && !pvNode && !in_check && ttAdjustedEval != static_eval &&
         ttAdjustedEval - rfpMargin >= beta + corrplexity * 20)
-        return ttAdjustedEval;
+        return (ttAdjustedEval + beta) / 2;
 
     // Null Move Pruning
     if (!ss->singular_move && !pvNode &&
