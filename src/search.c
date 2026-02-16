91d1dee7510ea2f469ada7bff860c73c06bbbbcf
Eren Araz <lerenarazl@gmail.com>
2026-02-16 17:20:27 +0300

    what about negative bonus :D bench: 6604886

--- src/search.c
+++ src/search.c
@@ -1701,7 +1701,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, SearchSta
     bool counter_move_available = counter_move ? !getMoveCapture(counter_move) && !getMovePromote(counter_move) : false;
 
     if (score <= originalAlpha && pos->move[myMIN(pos->ply - 1, maxPly - 1)] != 0 && counter_move_available) {
-        int pcm_bonus = myMIN(25 * depth, 512);
+        int pcm_bonus = -200;
 
         adjust_single_quiet_hist_entry(pos, pos->side, counter_move, pcm_bonus);
     }
