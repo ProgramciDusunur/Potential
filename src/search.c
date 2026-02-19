d5d39997c7e269b4ecb64a6a2daa705737f20702
Eren Araz <lerenarazl@gmail.com>
2026-02-19 20:48:19 +0300

    remove margin and do if ID has reasonable depth bench: 8298700

--- src/search.c
+++ src/search.c
@@ -1592,7 +1592,7 @@ int negamax(int alpha, int beta, int depth, board* pos, my_time* time, SearchSta
                 score = -negamax(-alpha - 1, -alpha, new_depth, pos, time, ss + 1, !cutNode);
 
                 // Post LMR search extension
-                if (!rootNode && doDeeper && score > alpha + 50) {
+                if (!rootNode && doDeeper && score > alpha && pos->rootDepth > 5) {
                     new_depth++;
                 }
             }
