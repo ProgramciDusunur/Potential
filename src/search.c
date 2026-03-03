7d5e6bf2abce4db91c4f3bfef3f9d71bd76434df
Eren Araz <lerenarazl@gmail.com>
2026-03-03 04:10:27 +0300

    tweak bench: 13177873

--- src/search.c
+++ src/search.c
@@ -1041,7 +1041,7 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
 
     // Null Move Pruning
     if (!ss->singular_move && depth >= NMP_DEPTH && !in_check && !rootNode &&
-            ttAdjustedEval >= beta + 30 &&
+            ttAdjustedEval >= beta + 120 &&
             pos->ply >= pos->nmpPly &&
             !justPawns(pos)) {
         struct copyposition copyPosition;
