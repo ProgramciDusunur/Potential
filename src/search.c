135a7bf9c33307e3340ed4db7b346596adcc7f20
Eren Araz <lerenarazl@gmail.com>
2026-03-16 16:13:16 +0300

    tweak bench: 10447011

--- src/search.c
+++ src/search.c
@@ -1618,7 +1618,7 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
         if (pvNode && (legal_moves == 1 || score > alpha)) {                   
             int full_depth_reduction = 0;
 
-            if (notTactical && legal_moves >= 2) {
+            if (notTactical && legal_moves >= 3) {
                 int moveHistoryReduction = moveHistory * 1024 / 16384;                
                 full_depth_reduction -= clamp(moveHistoryReduction, -3072, 3072);
             }
