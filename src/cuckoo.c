07073cf359b871abdafb47c7c1e71b4a335ada30
Eren Araz <lerenarazl@gmail.com>
2026-03-18 15:32:29 +0300

    time to introduce Cuckoo

---
+++ src/cuckoo.c
@@ -0,0 +1,11 @@
+#include "cuckoo.h"
+
+#define TABLE_SIZE 8192
+
+uint64_t keys[TABLE_SIZE];
+uint16_t moves[TABLE_SIZE];
+
+bool can_attack(uint8_t sq0, uint8_t sq1) {
+    bool can_attack = false;
+    return can_attack;
+}
