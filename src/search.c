46ee4a3f15c811449c737c303478154b38f3b8a8
Eren Araz <lerenarazl@gmail.com>
2026-03-20 17:17:00 +0300

    ``` Elo   | 2.13 +- 1.73 (95%) SPRT  | 8.0+0.08s Threads=1 Hash=32MB LLR   | -3.03 (-2.94, 2.94) [0.00, 7.50] Games | N: 73746 W: 22062 L: 21609 D: 30075 Penta | [2135, 8696, 14901, 8863, 2278] ``` https://rektdie.pythonanywhere.com/test/3922/
    ```
    Elo   | 0.29 +- 2.53 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=32MB
    LLR   | -2.97 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 34526 W: 10171 L: 10142 D: 14213
    Penta | [989, 4177, 6997, 4016, 1084]
    ```
    https://rektdie.pythonanywhere.com/test/3993/
    
    bench: 10682931

--- src/search.c
+++ src/search.c
@@ -1615,18 +1615,27 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
             score = -negamax(-alpha - 1, -alpha, new_depth, t, time, ss + 1, !cutNode);
         }
 
-        if (pvNode && (legal_moves == 1 || score > alpha)) {
-            if (!rootNode && currentMove == tt_move && tt_score < alpha && tt_flag == hashFlagBeta) {
-                new_depth -= 1;
+        if (pvNode && (legal_moves == 1 || score > alpha)) {                   
+            int full_depth_reduction = 0;
+
+            if (notTactical && legal_moves >= 3) {
+                int moveHistoryReduction = moveHistory * 1024 / 20480;
+                full_depth_reduction -= clamp(moveHistoryReduction, -3072, 3072);
             }
+
+            if (!rootNode && currentMove == tt_move && tt_score < alpha && tt_flag == hashFlagBeta) {
+                full_depth_reduction += 1024;
+            }
+            
+            int reduced_depth = new_depth - full_depth_reduction / 1024;
 
             // if we have chance about to dive into quiescence search then extend
             if (currentMove == tt_move && pos->rootDepth > 8 && tt_depth > 1) {
-                new_depth = myMAX(new_depth, 1);
+                reduced_depth = myMAX(reduced_depth, 1);
             }
-            
+
             // do normal alpha beta search
-            score = -negamax(-beta, -alpha, new_depth, t, time, ss + 1, false);
+            score = -negamax(-beta, -alpha, reduced_depth, t, time, ss + 1, false);        
         }
 
         // decrement ply
