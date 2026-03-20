38248fe219b2bdbdad3c70d2ea295ebeec7574b8
Eren Araz <lerenarazl@gmail.com>
2026-03-20 19:51:21 +0300

    add stuff

--- src/cuckoo.c
+++ src/cuckoo.c
@@ -6,6 +6,10 @@ uint64_t keys[TABLE_SIZE];
 uint16_t moves[TABLE_SIZE];
 
 bool can_attack(uint8_t sq0, uint8_t sq1) {
+    if (sq0 == sq1) {
+        return false;
+    }
+    
     bool can_attack = false;
     return can_attack;
 }
