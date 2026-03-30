680c9df9db267933a5fb08195c3fafb80b5a1ffc
Eren Araz <lerenarazl@gmail.com>
2026-03-31 01:53:49 +0300

    complete initial implementation and test time bench: 8924575

--- src/movepicker.h
+++ src/movepicker.h
@@ -1,3 +1,7 @@
 #include "structs.h"
 #include "values.h"
 #include "move.h"
+#include "search.h"
+
+void init_mp(MovePicker *mp, uint16_t tt_move);
+uint16_t get_next_move(MovePicker *mp, moves *moveList, int *move_scores, board *pos, ThreadData *t, SearchStack *ss);
