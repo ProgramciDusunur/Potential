4d94158204e30a74c885fba03e82891961fd1db6
Eren Araz <lerenarazl@gmail.com>
2026-03-31 19:27:49 +0300

    complete staged movegen implementation bench: 9650894

--- src/movepicker.h
+++ src/movepicker.h
@@ -4,4 +4,4 @@
 #include "search.h"
 
 void init_mp(MovePicker *mp, uint16_t tt_move);
-uint16_t get_next_move(MovePicker *mp, moves *moveList, int *move_scores, board *pos, ThreadData *t, SearchStack *ss);
+uint16_t get_next_move(MovePicker *mp, int *move_scores, board *pos, ThreadData *t, SearchStack *ss);
