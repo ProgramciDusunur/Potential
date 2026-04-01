a438e4a3a02de645f38e5473593ef8cb9bea2449
Eren Araz <lerenarazl@gmail.com>
2026-04-02 01:37:48 +0300

    don't play same move twice bench: 16433208

--- src/movepicker.c
+++ src/movepicker.c
@@ -20,7 +20,7 @@ uint16_t get_next_move(MovePicker *mp, int *move_scores, board *pos, ThreadData 
         // fallthrough
         case STAGE_KILLER:
             mp->CURRENT_STAGE = STAGE_GEN_NOISY;
-            if (t->search_d.killerMoves[pos->ply][0] != 0 && is_pseudo_legal(t->search_d.killerMoves[pos->ply][0], pos)) {
+            if (t->search_d.killerMoves[pos->ply][0] != 0 && t->search_d.killerMoves[pos->ply][0] != mp->tt_move && is_pseudo_legal(t->search_d.killerMoves[pos->ply][0], pos)) {
                 return t->search_d.killerMoves[pos->ply][0];
             }
         // fallthrough
