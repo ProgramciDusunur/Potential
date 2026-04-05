3db76fb12bd22281c8b5d5ba69343532c54e4fe2
Eren Araz <lerenarazl@gmail.com>
2026-04-05 14:39:37 +0300

    way lower margin and direct LTC test bench: 12040544

--- src/search.c
+++ src/search.c
@@ -1493,7 +1493,7 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
             extensions++;
 
             // Double Low Depth Extension
-            if (ttAdjustedEval <= alpha - 500) {
+            if (ttAdjustedEval <= alpha - 250) {
                 extensions++;    
             }
         }
