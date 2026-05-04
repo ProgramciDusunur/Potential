fc2d143d68afb719d5bc501e84c43293f78524f1
Eren Araz <lerenarazl@gmail.com>
2026-05-04 19:01:58 +0300

    just straight bonus bench: 11917299

--- src/search.c
+++ src/search.c
@@ -1748,9 +1748,9 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
     uint16_t prior_move = (ss - 1)->move;
     if (!rootNode && tt_flag == hashFlagBeta && prior_move && !isTactical(prior_move) &&
         (predicted_cut_node || pvNode)) {
-        int pcm_bonus = 5 + 25 * depth;
+        int pcm_bonus = 50;
 
-        pcm_bonus = myMIN(pcm_bonus, 2048);
+        //pcm_bonus = myMIN(pcm_bonus, 2048);
 
         adjust_single_quiet_hist_entry(t, pos->side, prior_move, pcm_bonus);
     }
