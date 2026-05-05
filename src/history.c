25cd4edd1c7abf9ec58944694c41460ddde90f2c
Eren Araz <lerenarazl@gmail.com>
2026-05-05 23:59:13 +0300

    let's go bench: 10745376

--- src/history.c
+++ src/history.c
@@ -43,6 +43,13 @@ void adjust_single_quiet_hist_entry(ThreadData *t, int side, uint16_t move, int 
     t->search_d.quietHistory[side][from][to][threatSource][threatTarget] += bonus;
 }
 
+void adjust_single_pawn_hist_entry(ThreadData *t, uint16_t move, int bonus) {
+    int from = getMoveSource(move);
+    int to = getMoveTarget(move);
+    
+    thread_pool.shared_history.pawnHistory[t->pos.pawnKey % 2048][t->pos.mailbox[from]][to] += bonus;
+}
+
 void updateQuietMoveHistory(ThreadData *t, uint16_t bestMove, int side, int bonus, moves *badQuiets) {
     int from = getMoveSource(bestMove);
     int to = getMoveTarget(bestMove);
