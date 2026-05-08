daa31a9f42f702cdefcb58818ebbc631f62e91c1
Eren Araz <lerenarazl@gmail.com>
2026-05-08 14:22:35 +0300

    tweak bench: 11515230

--- src/search.c
+++ src/search.c
@@ -1243,7 +1243,7 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
             } 
 
             else {
-                int noisy_futility_margin = static_eval + 71 * depth + moveHistory / 32;
+                int noisy_futility_margin = static_eval + 71 * depth + moveHistory / 96;
                 if (!in_check && depth <= 4 && mp.CURRENT_STAGE == STAGE_BAD_NOISY && noisy_futility_margin <= alpha) {
                     if (!is_decisive(bestScore) && bestScore < noisy_futility_margin) {
                         bestScore = noisy_futility_margin;
