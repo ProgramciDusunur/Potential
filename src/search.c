f13d4b3d49e1c3f00d6a5990a1971aa98f0e2f1b
Eren Araz <lerenarazl@gmail.com>
2026-05-24 04:21:58 +0300

    do double low depth singular extensions bench: 16814805

--- src/search.c
+++ src/search.c
@@ -1425,7 +1425,11 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
             ldse_margin += 768 * abs(correction_value) / 98304;
             if (ttAdjustedEval <= ldse_margin) {
                 extensions++;
-            }            
+            }
+
+            if (!tt_capture && tt_depth >= depth - 3 && ttAdjustedEval <= alpha - 150) {
+                extensions++;
+            }
         }
 
 
