683fc08ccbf473040e43de3064ef29cb37e28180
Eren Araz <lerenarazl@gmail.com>
2026-07-02 00:47:13 +0300

    don't increase search stack pointer bench: 18404363

--- src/search.c
+++ src/search.c
@@ -1263,7 +1263,7 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
     // Internal Iterative Deepening (IID)
     if (!rootNode && pvNode && depth >= 8 && !in_check && !ss->singular_move && !tt_move) {
         int idd_depth = (3 * depth - 7) / 4;
-        int idd_score = negamax(alpha, beta, idd_depth, t, time, ss + 1, true);
+        int idd_score = negamax(alpha, beta, idd_depth, t, time, ss, true);
 
         int16_t iid_score; uint8_t iid_depth, iid_flag; bool iid_pv;
         readHashEntry(pos, &tt_move, &iid_score, &iid_depth, &iid_flag, &iid_pv, pos->fifty);
