20bd6dd8e159956f4c4f1270de1efad508f97b72
Eren Araz <lerenarazl@gmail.com>
2026-07-02 12:33:19 +0300

    try only on non pv nodes bench: 18462004

--- src/search.c
+++ src/search.c
@@ -1261,8 +1261,8 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
     }
 
     // Internal Iterative Deepening (IID)
-    if (!rootNode && pvNode && depth >= 8 && !in_check && !ss->singular_move && !tt_move) {
-        int idd_depth = (3 * depth - 7) / 4;
+    if (!rootNode && !pvNode && depth >= 8 && !in_check && !ss->singular_move && !tt_move) {
+        int idd_depth = (768 * depth - 1792) / 1024;
         int idd_score = negamax(alpha, beta, idd_depth, t, time, ss, predicted_cut_node);
 
         int16_t iid_score; uint8_t iid_depth, iid_flag; bool iid_pv;
