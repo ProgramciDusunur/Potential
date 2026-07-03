4cb5e95af461da976d0c73fe8b3bc38259f42af9
Eren Araz <lerenarazl@gmail.com>
2026-07-03 03:28:46 +0300

    tweak bad noisy futility pruning bench: 18599094

--- src/search.c
+++ src/search.c
@@ -1355,8 +1355,8 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
             } 
 
             else {
-                int noisy_futility_margin = static_eval + BNFP_MARGIN * depth;
-                if (!in_check && depth <= 4 && mp.CURRENT_STAGE == STAGE_BAD_NOISY && noisy_futility_margin <= alpha) {
+                int noisy_futility_margin = static_eval + BNFP_MARGIN * lmrDepth;
+                if (!in_check && depth <= lmrDepth && mp.CURRENT_STAGE == STAGE_BAD_NOISY && noisy_futility_margin <= alpha) {
                     if (!is_decisive(bestScore) && bestScore < noisy_futility_margin) {
                         bestScore = noisy_futility_margin;
                     }
