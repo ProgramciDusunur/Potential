133d941fa9720ccb543894bd820ae5af6b345e45
Eren Araz <lerenarazl@gmail.com>
2026-07-04 00:30:18 +0300

    zero init for the values and another fixed node SPSA but this time 16 PPP bench: 19455924

--- src/search.c
+++ src/search.c
@@ -36,11 +36,11 @@
 /*╔════════════════════════════════╗
   ║    Prior Counter Move Bonus    ║
   ╚════════════════════════════════╝*/
-  TUNE_INT PCM_FACTOR = 85;
-  TUNE_INT PCM_DEPTH_MULT = 185;
-  TUNE_INT PCM_DEPTH_SUB = 41;
-  TUNE_INT PCM_LIMIT = 2514;
-  TUNE_INT PCM_DIVISOR = 134;
+  TUNE_INT PCM_FACTOR = 0;
+  TUNE_INT PCM_DEPTH_MULT = 0;
+  TUNE_INT PCM_DEPTH_SUB = 0;
+  TUNE_INT PCM_LIMIT = 2400;
+  TUNE_INT PCM_DIVISOR = 128;
 
 
 
