fd5dfb7215a8c93d4258b7c686cc8c58a6f9c755
Eren Araz <lerenarazl@gmail.com>
2026-07-04 01:24:49 +0300

    16 PPP 1.5k iters sane check bench: 18698748

--- src/search.c
+++ src/search.c
@@ -36,11 +36,11 @@
 /*╔════════════════════════════════╗
   ║    Prior Counter Move Bonus    ║
   ╚════════════════════════════════╝*/
-  TUNE_INT PCM_FACTOR = 0;
-  TUNE_INT PCM_DEPTH_MULT = 0;
-  TUNE_INT PCM_DEPTH_SUB = 0;
-  TUNE_INT PCM_LIMIT = 2400;
-  TUNE_INT PCM_DIVISOR = 128;
+  TUNE_INT PCM_FACTOR = 7;
+  TUNE_INT PCM_DEPTH_MULT = 2;
+  TUNE_INT PCM_DEPTH_SUB = 7;
+  TUNE_INT PCM_LIMIT = 2651;
+  TUNE_INT PCM_DIVISOR = 140;
 
 
 
