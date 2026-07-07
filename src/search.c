426cc35111ef494e99b40c6150d406444a1c0d12
Eren Araz <77514455+ProgramciDusunur@users.noreply.github.com>
2026-07-07 13:53:50 +0300

    do quad margin for bad noisy fp and do more deeper (#579)
    bench: 19310268

--- src/search.c
+++ src/search.c
@@ -1357,8 +1357,11 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
             } 
 
             else {
-                int noisy_futility_margin = static_eval + BNFP_MARGIN * depth;
-                if (!in_check && depth <= 4 && mp.CURRENT_STAGE == STAGE_BAD_NOISY && noisy_futility_margin <= alpha) {
+                int sdepth = myMAX(0, depth - 4);
+                int noisy_futility_margin = static_eval + BNFP_MARGIN * depth +
+                    sdepth * sdepth * 50;
+
+                if (!in_check && depth <= 8 && mp.CURRENT_STAGE == STAGE_BAD_NOISY && noisy_futility_margin <= alpha) {
                     if (!is_decisive(bestScore) && bestScore < noisy_futility_margin) {
                         bestScore = noisy_futility_margin;
                     }
