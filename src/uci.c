3db6433726d69e1894b52e92f5f579b388346ed6
Eren Araz <lerenarazl@gmail.com>
2026-07-31 13:09:44 +0300

    the whole data has been regenerated with the very first 64hl, 2m games, around 140-147m positions and 160 superbatches bench: 8389765

--- src/uci.c
+++ src/uci.c
@@ -21,7 +21,7 @@ extern _Atomic uint64_t total_fens_generated;
 extern _Atomic uint64_t games_played_count;
 extern uint64_t global_start_time;
 
-#define VERSION "4.5.4"
+#define VERSION "4.6.4"
 #define BENCH_DEPTH 13
 #define MAX_THREADS 512
 
