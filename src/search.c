26db7d4f9596ca403d83d06fe3377f5b747ba351
Eren Araz <lerenarazl@gmail.com>
2026-08-01 15:01:24 +0300

    an idea from Stockfish engine bench: 7947968

--- src/search.c
+++ src/search.c
@@ -951,7 +951,7 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
 
 
     int pvNode = beta - alpha > 1;
-
+    bool allNode  = !(pvNode || predicted_cut_node);
     int rootNode = pos->ply == 0;
 
     uint16_t bestMove = 0;
@@ -1573,6 +1573,10 @@ int negamax(int alpha, int beta, int depth, ThreadData *t, my_time* time, Search
 
         if (!improving && !in_check) {
             lmrReduction += IMPROVING_LMR_SCALAR;
+        }
+
+        if (allNode) {
+            lmrReduction += lmrReduction * 272 / (256 * depth + 256);
         }
 
         if (notTactical) {
